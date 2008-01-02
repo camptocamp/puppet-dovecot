b0b6a66b4e767f194fd9e1b8f489405aa1cb4eb2
mh <mh@d66ca3ae-40d7-4aa7-90d4-87d79ca94279>
2008-01-02 22:31:16 +0000

    fixed path of true
    git-svn-id: https://svn/ipuppet/trunk/modules/common@365 d66ca3ae-40d7-4aa7-90d4-87d79ca94279

--- manifests/classes/lsb_release.pp
+++ manifests/classes/lsb_release.pp
@@ -16,11 +16,20 @@
 # Exec[assert_lsbdistcodename] on the specific resource
 class assert_lsbdistcodename {
 
+	$true_exec = $operatingsystem ? {
+		openbsd => '/usr/bin/true',
+		default => '/bin/true'
+	}
+
+	$false_exec = $operatingsystem ? {
+		openbsd => '/usr/bin/false',
+		default => '/bin/false'
+	}
+
 	case $lsbdistcodename {
 		'': {
 			err("Please install lsb_release or set facter_lsbdistcodename in the environment of $fqdn")
-			# exec { "/bin/false # assert_lsbdistcodename": alias => assert_lsbdistcodename }
-			exec { "/bin/true # assert_lsbdistcodename": alias => assert_lsbdistcodename }
+			exec { "${true_exec} # assert_lsbdistcodename": alias => assert_lsbdistcodename }
 		}
 		'n/a': {
 			case $operatingsystem {
@@ -31,11 +40,11 @@ class assert_lsbdistcodename {
 					err("lsb_release was unable to report your distcodename; please set facter_lsbdistcodename in the environment of $fqdn")
 				}
 			}
-			exec { "/bin/false # assert_lsbdistcodename": alias => assert_lsbdistcodename }
+			exec { "${false_exec} # assert_lsbdistcodename": alias => assert_lsbdistcodename }
 		}
 		default: {
-			exec { "/bin/true # assert_lsbdistcodename": alias => assert_lsbdistcodename }
-			exec { "/bin/true # require_lsbdistcodename": alias => require_lsbdistcodename }
+			exec { "${true_exec} # assert_lsbdistcodename": alias => assert_lsbdistcodename }
+			exec { "${true_exec} # require_lsbdistcodename": alias => require_lsbdistcodename }
 		}
 	}
 
