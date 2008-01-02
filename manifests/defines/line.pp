af090d3df46b4365059d7915ba27b25c28638fb9
mh <mh@d66ca3ae-40d7-4aa7-90d4-87d79ca94279>
2008-01-02 22:22:06 +0000

    fixed openbsd path
    git-svn-id: https://rfd.cronopios.org/ipuppet/trunk/modules/common@364 d66ca3ae-40d7-4aa7-90d4-87d79ca94279

--- manifests/defines/line.pp
+++ manifests/defines/line.pp
@@ -24,16 +24,20 @@
 #
 #
 define line($file, $line, $ensure = 'present') {
+	$grep = $operatingsystem ? {
+		openbsd => '/usr/bin/grep',
+		default => '/bin/grep',
+	}
 	case $ensure {
 		default : { err ( "unknown ensure value '${ensure}'" ) }
 		present: {
 			exec { "/bin/echo '${line}' >> '${file}'":
-				unless => "/bin/grep -qFx '${line}' '${file}'"
+				unless => "${grep} -qFx '${line}' '${file}'"
 			}
 		}
 		absent: {
 			exec { "/usr/bin/perl -ni -e 'print unless /^\\Q${line}\\E\$/' '${file}'":
-				onlyif => "/bin/grep -qFx '${line}' '${file}'"
+				onlyif => "${grep} -qFx '${line}' '${file}'"
 			}
 		}
 	}
