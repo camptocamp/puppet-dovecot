1192a201b59d3571ca0dd70ca3d0614fde1b7f78
Marcel Haerry <haerry@puzzle.ch>
2008-04-15 16:36:46 +0200

    empty module $MODULENAME

---
+++ manifests/init.pp
@@ -0,0 +1,9 @@
+#######################################
+# common module
+# Puzzle ITC - haerry+puppet(at)puzzle.ch
+# GPLv3
+#######################################
+
+
+# modules_dir { "common": }
+class common {}
