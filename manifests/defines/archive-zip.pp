6e2a9683649d8156babef367a25c81c97fd6bbaf
Francois Deppierraz <francois.deppierraz@camptocamp.com>
2008-11-28 10:57:30 +0100

    New definitions:
      * The old and (in)famous append_if_no_such_line
      * archive::tar-gz
      * archive::zip
      * An obsolete slightly modified concatened_file which is probably going to
        disappear

---
+++ manifests/defines/archive-zip.pp
@@ -0,0 +1,6 @@
+define common::archive::zip($source, $target) {
+  exec {"$name unpack":
+    command => "TMPFILE=\$(mktemp); curl -o \$TMPFILE.zip ${source} && unzip \$TMPFILE.zip -d ${target} && rm \$TMPFILE && rm \$TMPFILE.zip && touch ${name}",
+    creates => $name,
+  }
+}
