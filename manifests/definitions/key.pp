40c861674443ed95046ed531594ab273cae44863
Francois Deppierraz <francois.deppierraz@camptocamp.com>
2009-02-27 14:34:31 +0100

    Use $name instead of $keyid and implement ensure => absent

--- manifests/definitions/key.pp
+++ manifests/definitions/key.pp
@@ -1,8 +1,8 @@
-define apt::key($ensure = present, $source, $keyid) {
+define apt::key($ensure = present, $source) {
   case $ensure {
     present: {
       exec { "/usr/bin/wget -O - '$source' | /usr/bin/apt-key add -":
-        unless => "apt-key list | grep -Fqe '$keyid'",
+        unless => "apt-key list | grep -Fqe '${name}'",
         path   => "/bin:/usr/bin",
         before => Exec["apt-get_update"],
         notify => Exec["apt-get_update"],
@@ -10,7 +10,9 @@ define apt::key($ensure = present, $source, $keyid) {
     }
     
     absent: {
-      notice "Sorry, not yet implemented"
+      exec {"/usr/bin/apt-key del ${name}":
+        onlyif => "apt-key list | grep -Fqe '${name}'",
+      }
     }
   }
 }
