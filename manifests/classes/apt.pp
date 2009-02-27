f0324cf29499ff063b95a2a7033a2c90063d8ead
Francois Deppierraz <francois.deppierraz@camptocamp.com>
2009-02-27 15:45:27 +0100

    define the apt-get update exec in that module instead of os

--- manifests/classes/apt.pp
+++ manifests/classes/apt.pp
@@ -1,6 +1,11 @@
 class apt {
-  file{"/etc/apt/apt.conf.d/10periodic":
+  apt::confg {"10periodic":
     ensure => present,
     source => "puppet:///apt/10periodic",
+  }
+
+  exec { "apt-get_update":
+    command => "apt-get update",
+    refreshonly => true,
   }
 }
