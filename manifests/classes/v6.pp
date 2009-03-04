888b50842d3ce7464c44a18f76ef8a7185d3cf80
Francois Deppierraz <francois.deppierraz@camptocamp.com>
2009-03-04 18:45:58 +0100

    initial commit

---
+++ manifests/classes/v6.pp
@@ -0,0 +1,54 @@
+class java::v6 {
+
+  case $lsbdistcodename {
+    'etch' : {
+      os::backported_package { 
+        "sun-java6-bin":    
+          ensure       => present,
+          responsefile => "/var/cache/debconf/sun-java6-bin.preseed",
+          require      => [Exec["apt-get_update"], File["/var/cache/debconf/sun-java6-bin.preseed"]];
+        "sun-java6-jdk":    
+          ensure  => present,
+          require => Package["sun-java6-bin"];
+        "sun-java6-jre":    
+          ensure  => present,
+          require => Package["sun-java6-bin"];
+      }
+    }
+    'lenny' : {
+      package { "sun-java6-bin":
+        ensure       => present,
+        responsefile => "/var/cache/debconf/sun-java6-bin.preseed",
+        require      => [Exec["apt-get_update"], File["/var/cache/debconf/sun-java6-bin.preseed"]],
+      }
+      
+      package {["sun-java6-jdk", "sun-java6-jre"]:
+        ensure  => present,
+        require => Package["sun-java6-bin"],
+      }
+    } 
+  }
+
+
+  case $architecture {
+    amd64: {
+      os::backported_package {"sun-java6-plugin":
+        ensure  => present,
+        require => Package["sun-java6-bin"],
+      }
+    }
+  }
+
+  # Thanks to Java strange licensing
+  file {"/var/cache/debconf/sun-java6-bin.preseed":
+    ensure  => present,
+    content => "sun-java6-bin   shared/accepted-sun-dlj-v1-1    boolean true",
+  }
+
+  $jvm = '6'
+  file {"/etc/profile.d/java_home":
+    ensure => present,
+    content => template("java/java-home.erb"),
+    require => File["/etc/profile.d"],
+  }
+}
