a20c474b8e78657f0bc838b73065e5e943098ee3
Mathieu Bornoz <mbornoz@sa.camptocamp.com>
2009-03-09 13:57:36 +0100

    first commit

---
+++ manifests/classes/base.pp
@@ -0,0 +1,30 @@
+class mapfish::base {
+
+  case $lsbdistcodename {
+    'etch' : {
+      os::backported_package {"libapache2-mod-wsgi":
+        ensure => present,
+      }
+
+      package{"maven2":
+        ensure  => present,
+        require => Os::Backported_package["sun-java6-jdk"],
+      }
+    }
+    'lenny' : {
+      package {"libapache2-mod-wsgi":
+        ensure => present,
+      }
+
+      package {"maven2":
+        ensure  => present,
+        require => Package["sun-java6-jdk"],
+      }
+    }
+  }
+
+  apache::module{ "headers":
+    ensure  => present,
+  }
+
+}
