d53e92f9af9293cdf225d64ed81ddd1a46531daf
Francois Deppierraz <francois.deppierraz@camptocamp.com>
2009-03-11 13:58:13 +0100

    group puppet workarounds inside c2c::puppet-fix

---
+++ manifests/classes/puppet-fix.pp
@@ -0,0 +1,6 @@
+class c2c::puppet-fix {
+  file {"/usr/lib/ruby/1.8/puppet/type/file/selcontext.rb":
+    ensure => present,
+    source => "puppet:///c2c/puppet-fix/selcontext-with-patch-for-1852.rb",
+  }
+}
