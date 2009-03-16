fbbf0a0ba1915c02f80d8d786d422a53b371c5d2
Marc Fournier <marc.fournier@camptocamp.com>
2009-03-16 15:23:50 +0100

    bugfix in backup script

--- files/mysql-backup.sh
+++ files/mysql-backup.sh
@@ -23,7 +23,7 @@ BKPDIR="/var/backups/mysql"
 # Installed ?
 if [ -e /usr/bin/mysqladmin ]; then
   # used ?
-  if [ -d /var/lib/mysql ] && [ -z "$(find /var/lib/mysql -maxdepth 1 -type d ! -iname mysql )" ]; then
+  if [ -d /var/lib/mysql ] && [ -n "$(find /var/lib/mysql -maxdepth 1 -type d ! -iname mysql ! -iname test )" ]; then
     # Running ?
     if /usr/bin/mysqladmin -uroot ping > /dev/null 2>&1; then
       /usr/bin/mysqldump --all-database |gzip > $BKPDIR/mysql-$DAY.sql.gz
@@ -31,7 +31,11 @@ if [ -e /usr/bin/mysqladmin ]; then
       echo 'mysqld not running'
       exit 1
     fi
+  else
+    # no databases to backup ? no problem
+    exit 0
   fi
+else
+  # not installed ? no problem...
+  exit 0
 fi
-exit 0
-
