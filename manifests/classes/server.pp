31a71fd7fc2192375f7d8b7973531aefa978ed80
Marc Fournier <marc.fournier@camptocamp.com>
2009-03-17 15:45:52 +0100

    removed duplicate code

--- manifests/classes/server.pp
+++ manifests/classes/server.pp
@@ -213,23 +213,4 @@ class mysql::server {
     }
   }
 
-  define rights($database, $user, $password, $ensure=true) {
-    case $ensure {
-      present:{
-        exec {"Set rigths for db ${database}":
-          command => "mysql -e \"GRANT ALL PRIVILEGES on ${database}.* TO ${user}@localhost IDENTIFIED BY '${password}';\"",
-          unless  => "mysql --database=${database} --user=${user} --password=${password} -A -e 'exit'",
-          require => Exec["Set MySQL server root password"],
-        }
-      }
-
-      absent:{
-        exec {"unsert rights for db ${database}":
-          command => "mysql -e \"REVOKE ALL PRIVILEGES on ${database}.* FROM ${user}@localhost;\"",
-          onlyif  => "mysql --database=${database} --user=${user} --password=${password} -A -e 'exit'",
-          require => Exec["Set MySQL server root password"],
-        }
-      }
-    }
-  }
 }
