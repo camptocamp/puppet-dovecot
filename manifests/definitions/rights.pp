8353ce43ef74aa942e970485937e991b5abccb54
Marc Fournier <marc.fournier@camptocamp.com>
2009-07-15 12:09:31 +0200

    mysql: native mysql types dont like $ensure

--- manifests/definitions/rights.pp
+++ manifests/definitions/rights.pp
@@ -1,14 +1,12 @@
 define mysql::rights($database, $user, $password, $host="localhost", $ensure="present") {
 
-  if $mysql_exists == "true" {
+  if $mysql_exists == "true" and $ensure == "present" {
     mysql_user { "${user}@${host}":
-      ensure => $ensure,
       password_hash => mysql_password($password),
       require => File["/root/.my.cnf"],
     }
 
     mysql_grant { "${user}@${host}/${database}":
-      ensure => $ensure,
       privileges => "all",
       require => File["/root/.my.cnf"],
     }
