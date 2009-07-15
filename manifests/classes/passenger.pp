eb923318adf0c344ecc2db87cb5eb905d97dc0c7
Marc Fournier <marc.fournier@camptocamp.com>
2009-07-15 12:00:21 +0200

    ruby: classes to install passenger (mod_rails)

---
+++ manifests/classes/passenger.pp
@@ -0,0 +1,59 @@
+
+# you'll probably want to include "ruby::gems" along with this
+
+class ruby::passenger {
+
+  $passenger_version = "2.2.4"
+  $passenger_root = $operatingsystem ? {
+    RedHat => "/usr/lib/ruby/gems/1.8/gems/passenger-${passenger_version}",
+    # Debian => TODO
+  }
+
+  package { "passenger":
+    ensure   => $passenger_version,
+    provider => "gem",
+    require  => Package["ruby-dev"],
+  }
+
+}
+
+class ruby::passenger::apache inherits ruby::passenger {
+
+  package { "apache-dev":
+    ensure => present,
+    name   => $operatingsystem ? {
+      RedHat => "httpd-devel",
+      Debian => "apache2-threaded-dev",
+    },
+  }
+
+  exec { "install passenger":
+    command     => "passenger-install-apache2-module --auto",
+    subscribe   => Package["passenger"],
+    refreshonly => true,
+    require     => Package[["passenger", "apache-dev"]],
+  }
+
+  file { "passenger.load":
+    ensure  => present,
+    mode    => 0644,
+    owner   => "root",
+    group   => "root",
+    seltype => "httpd_config_t",
+    name    => $operatingsystem ? {
+      RedHat => "/etc/httpd/mods-available/passenger.load",
+      Debian => "/etc/apache2/mods-available/passenger.load",
+    },
+    require => [Exec["install passenger"], Package["apache"]],
+    content => "# file managed by puppet
+LoadModule passenger_module ${passenger_root}/ext/apache2/mod_passenger.so
+PassengerRoot ${passenger_root}
+PassengerRuby /usr/bin/ruby
+",
+  }
+
+}
+
+class ruby::passenger::nginx inherits ruby::passenger {
+    # TODO: passenger-install-nginx-module --auto
+}
