02ca9de75f445965297d317a5cfa7bf321c995ea
Marc Fournier <marc.fournier@camptocamp.com>
2009-07-16 11:38:18 +0200

    ruby: passenger needs a c++ compiler

--- manifests/classes/passenger.pp
+++ manifests/classes/passenger.pp
@@ -33,7 +33,7 @@ class ruby::passenger::apache inherits ruby::passenger {
     command     => "${passenger_root}/bin/passenger-install-apache2-module --auto",
     subscribe   => Package["passenger"],
     refreshonly => true,
-    require     => Package[["passenger", "apache-dev"]],
+    require     => [Package[["passenger", "apache-dev"]], Class["buildenv::cpp"]],
   }
 
   file { "passenger.load":
