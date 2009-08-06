camptocamp/puppet-dovecot

commit cad6d5e040fd1df09336afced44aac73bb9ee9fa

Add support for remote databases

manifests/definitions/user.pp
@@ -1,5 +1,6 @@
 define postgresql::user($ensure, $password = false, $superuser = false, 
-                        $createdb = false, $createrole = false) {
+                        $createdb = false, $createrole = false,
+                        $hostname = '/var/run/postgresql', $port = '5432', $user = 'postgres') {
 
   $pgpass = $password ? {
     false   => "",
@@ -21,6 +22,9 @@ define postgresql::user($ensure, $password = false, $superuser = false,
     default => "CREATEROLE",
   }
 
+  # Connection string
+  $connection = "-h ${hostname} -p ${port} -U ${user}"
+
   case $ensure {
     present: {
 
@@ -28,41 +32,41 @@ define postgresql::user($ensure, $password = false, $superuser = false,
       # User with '-' like www-data must be inside double quotes
       exec { "Create postgres user $name":
         command => $password ? {
-          false => "psql -c \"CREATE USER \\\"$name\\\" \" ",
-          default => "psql -c \"CREATE USER \\\"$name\\\" PASSWORD '$password'\" ",
+          false => "psql ${connection} -c \"CREATE USER \\\"$name\\\" \" ",
+          default => "psql ${connection} -c \"CREATE USER \\\"$name\\\" PASSWORD '$password'\" ",
         },
         user    => "postgres",
-        unless  => "psql -c '\\du' | grep '^  *$name'",
+        unless  => "psql ${connection} -c '\\du' | grep '^  *$name'",
         require => User["postgres"],
       }
 
       exec { "Set SUPERUSER attribute for postgres user $name":
-        command => "psql -c 'ALTER USER \"$name\" $superusertext' ",
+        command => "psql ${connection} -c 'ALTER USER \"$name\" $superusertext' ",
         user    => "postgres",
-        unless  => "psql -tc \"SELECT rolsuper FROM pg_roles WHERE rolname = '$name'\" |grep -q $(echo $superuser |cut -c 1)",
+        unless  => "psql ${connection} -tc \"SELECT rolsuper FROM pg_roles WHERE rolname = '$name'\" |grep -q $(echo $superuser |cut -c 1)",
         require => [User["postgres"], Exec["Create postgres user $name"]],
       }
 
       exec { "Set CREATEDB attribute for postgres user $name":
-        command => "psql -c 'ALTER USER \"$name\" $createdbtext' ",
+        command => "psql ${connection} -c 'ALTER USER \"$name\" $createdbtext' ",
         user    => "postgres",
-        unless  => "psql -tc \"SELECT rolcreatedb FROM pg_roles WHERE rolname = '$name'\" |grep -q $(echo $createdb |cut -c 1)",
+        unless  => "psql ${connection} -tc \"SELECT rolcreatedb FROM pg_roles WHERE rolname = '$name'\" |grep -q $(echo $createdb |cut -c 1)",
         require => [User["postgres"], Exec["Create postgres user $name"]],
       }
 
       exec { "Set CREATEROLE attribute for postgres user $name":
-        command => "psql -c 'ALTER USER \"$name\" $createroletext' ",
+        command => "psql ${connection} -c 'ALTER USER \"$name\" $createroletext' ",
         user    => "postgres",
-        unless  => "psql -tc \"SELECT rolcreaterole FROM pg_roles WHERE rolname = '$name'\" |grep -q $(echo $createrole |cut -c 1)",
+        unless  => "psql ${connection} -tc \"SELECT rolcreaterole FROM pg_roles WHERE rolname = '$name'\" |grep -q $(echo $createrole |cut -c 1)",
         require => [User["postgres"], Exec["Create postgres user $name"]],
       }
 
       if $password { 
-        # change only if its not the same password
+        # change only if it's not the same password
         exec { "Change password for postgres user $name":
-          command => "psql -c \"ALTER USER \\\"$name\\\" PASSWORD '$password' \"",
+          command => "psql ${connection} -c \"ALTER USER \\\"$name\\\" PASSWORD '$password' \"",
           user    => "postgres",
-          unless  => "TMPFILE=$(mktemp /tmp/.pgpass.XXXXXX) && echo 'localhost:5432:template1:$name:$pgpass' > \$TMPFILE && PGPASSFILE=\$TMPFILE psql -h localhost -c '\\q' -U $name template1 && rm -f \$TMPFILE",
+          unless  => "TMPFILE=$(mktemp /tmp/.pgpass.XXXXXX) && echo 'localhost:${port}:template1:$name:$pgpass' > \$TMPFILE && PGPASSFILE=\$TMPFILE psql ${connection} -c '\\q' -U $name template1 && rm -f \$TMPFILE",
           require => [User["postgres"], Exec["Create postgres user $name"]],
         }
       }
@@ -71,9 +75,9 @@ define postgresql::user($ensure, $password = false, $superuser = false,
 
     absent:  {
       exec { "Remove postgres user $name":
-        command => "psql -c 'DROP USER \"$name\" ' ",
+        command => "psql ${connection} -c 'DROP USER \"$name\" ' ",
         user    => "postgres",
-        onlyif  => "psql -c '\\du' | grep '$name  *|'"
+        onlyif  => "psql ${connection} -c '\\du' | grep '$name  *|'"
       }
     }
   

commit b5012739d3ce1b78463d10f87bd0859f7f0f28e4

postgresql::user: fixed issue introduced in cad6d5e0
The long "unless" one-liner checked password match using
"psql -h /var/run/postgresql" instead of "psql -h localhost".

As this didn't work, puppet was applying the resource
Exec["Change password for postgres user $name"] each time.

manifests/definitions/user.pp
@@ -1,4 +1,4 @@
-define postgresql::user($ensure, $password = false, $superuser = false, 
+define postgresql::user($ensure, $password = false, $superuser = false,
                         $createdb = false, $createrole = false,
                         $hostname = '/var/run/postgresql', $port = '5432', $user = 'postgres') {
 
@@ -61,12 +61,17 @@ define postgresql::user($ensure, $password = false, $superuser = false,
         require => [User["postgres"], Exec["Create postgres user $name"]],
       }
 
-      if $password { 
+      if $password {
+        $host = $hostname ? {
+          '/var/run/postgresql' => "localhost",
+          default               => $hostname,
+        }
+
         # change only if it's not the same password
         exec { "Change password for postgres user $name":
           command => "psql ${connection} -c \"ALTER USER \\\"$name\\\" PASSWORD '$password' \"",
           user    => "postgres",
-          unless  => "TMPFILE=$(mktemp /tmp/.pgpass.XXXXXX) && echo 'localhost:${port}:template1:$name:$pgpass' > \$TMPFILE && PGPASSFILE=\$TMPFILE psql ${connection} -c '\\q' -U $name template1 && rm -f \$TMPFILE",
+          unless  => "TMPFILE=$(mktemp /tmp/.pgpass.XXXXXX) && echo '${host}:${port}:template1:${name}:${pgpass}' > \$TMPFILE && PGPASSFILE=\$TMPFILE psql -h ${host} -p ${port} -U ${name} -c '\q' template1 && rm -f \$TMPFILE",
           require => [User["postgres"], Exec["Create postgres user $name"]],
         }
       }
@@ -80,7 +85,7 @@ define postgresql::user($ensure, $password = false, $superuser = false,
         onlyif  => "psql ${connection} -c '\\du' | grep '$name  *|'"
       }
     }
-  
+
     default: {
       fail "Invalid 'ensure' value '$ensure' for postgres::user"
       }