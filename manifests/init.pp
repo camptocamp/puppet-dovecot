ef591642c392772c2b71c90577509598124985c1
Marc Fournier <marc.fournier@camptocamp.com>
2009-08-17 18:13:19 +0200

    subversion: avoid saving user password
     - new augeas lens to edit /etc/subversion/{config,servers}
     - defaults to NOT save passwords in ~/.subversion/auth/...
     - added some rdoc

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,6 +5,8 @@
 # forked by Puzzle ITC
 # Marcel Härry haerry+puppet(at)puzzle.ch
 # Simon Josi josi+puppet(at)puzzle.ch
+# forked by Camptocamp SA
+# Marc Fournier, marc.fournier(at)camptocamp.com
 # 
 # Permission to use, copy, modify, and/or distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
@@ -21,6 +23,15 @@
 
 # modules_dir { \"subversion\": }
 
+#
+# == Class: subversion
+#
+# Installs subversion and configures the client to NOT store repository
+# passwords in users home directories. This can still be overriden by editing
+# ~/.subversion/config
+#
+# For more details, have a look at: http://svnbook.red-bean.com/nightly/en/svn.advanced.confarea.html#svn.advanced.confarea.opts.config
+#
 class subversion {
     case $operatingsystem {
         debian,ubuntu: { include subversion::debian }
@@ -31,6 +42,18 @@ class subversion {
 class subversion::base {
     package{'subversion':
         ensure => present,
+    }
+
+    file { "/usr/share/augeas/lenses/contrib/subversion.aug":
+        ensure => present,
+        source => "puppet:///subversion/subversion.aug",
+    }
+
+    augeas { "avoid svn password saving":
+        load_path => "/usr/share/augeas/lenses/contrib/",
+        context   => "/files/etc/subversion/config/auth/",
+        require   => File["/usr/share/augeas/lenses/contrib/subversion.aug"],
+        changes   => "set store-password no",
     }
 }
 class subversion::debian inherits subversion::base {
