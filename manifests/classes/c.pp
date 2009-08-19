a9c247671c7f63f588ea14865cab0e5bad348de5
Marc Fournier <marc.fournier@camptocamp.com>
2009-08-19 13:00:48 +0200

    buildenv::c - ensure libc-dev gets installed

--- manifests/classes/c.pp
+++ manifests/classes/c.pp
@@ -2,4 +2,13 @@ class buildenv::c {
 
   package { ["make", "gcc", "cpp", "autoconf", "automake", "m4", "bison"]: }
 
+  package { "libc-dev":
+    ensure => present,
+    name   => $operatingsystem ? {
+      Debian => "libc6-dev",
+      Ubuntu => "libc6-dev",
+      RedHat => "glibc-devel",
+    },
+  }
+
 }
