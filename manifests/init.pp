cd52e802e5392d4c01ca9e723cc937c8c25bb879
Marc Fournier <marc.fournier@camptocamp.com>
2009-08-27 17:30:51 +0200

    svn: compatibility with older redhat's

--- manifests/init.pp
+++ manifests/init.pp
@@ -44,16 +44,26 @@ class subversion::base {
         ensure => present,
     }
 
+    file { '/etc/subversion':
+        ensure  => directory,
+        require => Package['subversion'],
+    }
+
     file { "/usr/share/augeas/lenses/contrib/subversion.aug":
         ensure => present,
         source => "puppet:///subversion/subversion.aug",
     }
 
+    # only recent version of svn support the "store-password" option.
     augeas { "avoid svn password saving":
         load_path => "/usr/share/augeas/lenses/contrib/",
         context   => "/files/etc/subversion/config/auth/",
-        require   => File["/usr/share/augeas/lenses/contrib/subversion.aug"],
-        changes   => "set store-password no",
+        require   => [File["/usr/share/augeas/lenses/contrib/subversion.aug"], File["/etc/subversion"]],
+        changes   => $lsbdistcodename ? {
+            default => "set store-auth-creds no",
+            Tikanga => "set store-password no",
+            lenny   => "set store-password no",
+        },
     }
 }
 class subversion::debian inherits subversion::base {
