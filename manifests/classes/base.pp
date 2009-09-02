fbf15828917c1f1bf4759d121d2b496a12a92288
Mathieu Bornoz <mathieu.bornoz@camptocamp.com>
2009-09-02 08:39:57 +0200

    maven is already defined in module java::dev

--- manifests/classes/base.pp
+++ manifests/classes/base.pp
@@ -5,11 +5,6 @@ class mapfish::base {
       os::backported_package {"libapache2-mod-wsgi":
         ensure => present,
       }
-
-      package{"maven2":
-        ensure  => present,
-        require => Os::Backported_package["sun-java6-jdk"],
-      }
     }
     'lenny' : {
       package {"libapache2-mod-wsgi":
@@ -18,11 +13,6 @@ class mapfish::base {
 
       package {"python-virtualenv":
         ensure => present,
-      }
-
-      package {"maven2":
-        ensure  => present,
-        require => Package["sun-java6-jdk"],
       }
 
       package {"libgeos-dev":
