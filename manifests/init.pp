9e3fcd3d407a05811c84c2ef4dcbdde8b970cbd8
Marc Fournier <marc.fournier@camptocamp.com>
2009-10-14 17:44:54 +0200

    pacemaker: new class pacemaker::apache::ssl

--- manifests/init.pp
+++ manifests/init.pp
@@ -159,6 +159,35 @@ class pacemaker::apache {
   }
 }
 
+/*
+
+== Class: pacemaker::apache::ssl
+
+Companion class for pacemaker::apache
+
+Requires:
+- apache's puppet module
+
+Example usage:
+  include pacemaker::apache
+  include pacemaker::apache::ssl
+  apache::vhost {$fqdn: ensure => present }
+
+*/
+class pacemaker::apache::ssl {
+
+  case $operatingsystem {
+
+    RedHat: {
+      include apache::ssl::redhat
+    }
+
+    Debian: {
+      include apache::ssl::debian
+    }
+  }
+}
+
 
 /*
 
