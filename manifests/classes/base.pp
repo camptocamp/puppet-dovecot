479365b1f3d5ad4eb001b41e1e1e38d7b67bafd9
mbornoz <mathieu.bornoz@camptocamp.com>
2009-11-10 09:30:38 +0100

    force utf8 only for lenny
    moved this workaround from base to lenny

--- manifests/classes/base.pp
+++ manifests/classes/base.pp
@@ -34,11 +34,4 @@ class postgresql::base {
     require => [User["postgres"], File["/usr/local/bin/pgsql-backup.sh"]],
   }
 
-  exec {"force UTF8 as default encoding" : 
-    command   => "echo \"UPDATE pg_database SET datistemplate = FALSE where datname = 'template1';drop database template1;create database template1 with template = template0 encoding = 'UTF8';UPDATE pg_database SET datistemplate = TRUE where datname = 'template1';\" |psql",
-    unless    => "psql -l |grep template1 |grep -q UTF8",
-    user      => postgres,
-    require   => [Package["postgresql"], User["postgres"]],
-  }
-
 }
--- manifests/classes/v8-3.pp
+++ manifests/classes/v8-3.pp
@@ -36,6 +36,17 @@ class postgresql::v8-3 inherits postgresql::base {
         require   => Package["postgresql-8.3"],
         alias     => "postgresql",
       }
+  
+      # [workaround]
+      # by default pg_createcluster encoding derived from locale
+      # but it do does not work by installing postgresql via puppet ?!
+      exec {"force UTF8 as default encoding" : 
+        command   => "echo \"UPDATE pg_database SET datistemplate = FALSE where datname = 'template1';drop database template1;create database template1 with template = template0 encoding = 'UTF8';UPDATE pg_database SET datistemplate = TRUE where datname = 'template1';\" |psql",
+        unless    => "psql -l |grep template1 |grep -q UTF8",
+        user      => postgres,
+        require   => Service["postgresql-8.3"],
+      }
+
     }
     default: {
       fail "postgresql version not available for ${operatingsystem}/${lsbdistcodename}"
