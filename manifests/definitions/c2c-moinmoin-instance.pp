9d45fd8a783b82b10857aa27a0fedc9f353fb9ef
Cedric Jeanneret <cedric.jeanneret@camptocamp.com>
2009-12-08 15:20:03 +0100

    added "ensure" arg

--- manifests/definitions/c2c-moinmoin-instance.pp
+++ manifests/definitions/c2c-moinmoin-instance.pp
@@ -1,14 +1,15 @@
-define c2c::moinmoininstance($vhost, $urlmatch, $acl_before=false, $acl_enabled=false) {
+define c2c::moinmoininstance($ensure=present, $vhost, $urlmatch, $acl_before=false, $acl_enabled=false) {
   moinmoin::instance { "$name":
-    vhost => "$vhost",
-    urlmatch => "$urlmatch",
-    replace => true,
-    acl_before => $acl_before,
+    vhost       => "$vhost",
+    urlmatch    => "$urlmatch",
+    replace     => true,
+    acl_before  => $acl_before,
     acl_enabled => $acl_enabled,
+    ensure      => $ensure,
   }
   apache::moinmoin::instance::auth { "$name":
-    vhost => "$vhost",
-    ensure => present,
+    vhost  => "$vhost",
+    ensure => $ensure,
   }
 
 }
