a20c8eb134d45dcc3a7b628be27273b03b5ff05a
Marc Fournier <marc.fournier@camptocamp.com>
2009-12-23 10:05:56 +0100

    monitoring::syslog - syslog server has syslog-ng

--- manifests/classes/syslog.pp
+++ manifests/classes/syslog.pp
@@ -1,21 +1,32 @@
 class monitoring::syslog {
 
-  monitoring::check { "Process: syslogd":
-    codename => "check_syslogd_process",
-    command  => "check_procs",
-    options  => $operatingsystem ? {
-      Debian => "-w 1:1 -c 1:1 -C rsyslogd",
-      RedHat => "-w 1:1 -c 1:1 -C syslogd",
-    },
-  }
+  if $ipaddress == $syslog_server {
 
-  if $operatingsystem == "RedHat" {
-
-    monitoring::check { "Process: klogd":
-      codename => "check_klogd_process",
+    monitoring::check { "Process: syslogd":
+      codename => "check_syslogd_process",
       command  => "check_procs",
-      options  => "-w 1:1 -c 1:1 -C klogd",
+      options  => "-w 1:1 -c 1:1 -C syslog-ng",
     }
-  }
 
+  } else {
+
+    monitoring::check { "Process: syslogd":
+      codename => "check_syslogd_process",
+      command  => "check_procs",
+      options  => $operatingsystem ? {
+        Debian => "-w 1:1 -c 1:1 -C rsyslogd",
+        RedHat => "-w 1:1 -c 1:1 -C syslogd",
+      },
+    }
+
+    if $operatingsystem == "RedHat" {
+
+      monitoring::check { "Process: klogd":
+        codename => "check_klogd_process",
+        command  => "check_procs",
+        options  => "-w 1:1 -c 1:1 -C klogd",
+      }
+    }
+
+  }
 }
