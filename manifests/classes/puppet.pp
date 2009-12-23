5c975b34643aaa3a698f65ee19906c6b7cf1010c
Marc Fournier <marc.fournier@camptocamp.com>
2009-12-23 18:01:27 +0100

    monitoring::puppet - monitoring puppet runs.

---
+++ manifests/classes/puppet.pp
@@ -0,0 +1,34 @@
+class monitoring::puppet {
+
+  # Le ficher state.yaml est mis à jour à chaque run puppet. Si puppet n'est
+  # plus exécuté (cron absent, fichier de lock), le fichier n'est plus mis à
+  # jour. Si puppet a été interrompu (CTRL-C), il sera vide.
+  monitoring::check { "Puppet last run":
+    codename => "check_puppet_state_yaml",
+    command  => "check_file_age",
+    options  => "-C 1 -c 86400 -f /var/puppet/state/state.yaml", # once per day
+    interval => "360", # 6h
+    retry    => "180", # 3h
+  }
+
+  # Si le manifest puppet fourni par le puppetmaster génère une erreur ou un
+  # warning, puppet appliquera la config stockée dans localconfig.yaml à la
+  # place. Ce fichier ne sera donc pas mis à jour.
+  monitoring::check { "Puppet last manifest refresh":
+    codename => "check_puppet_localconfig_yaml",
+    command  => "check_file_age",
+    options  => "-c 259200 -f /var/puppet/state/localconfig.yaml", # once every 3 days
+    interval => "360", # 6h
+    retry    => "180", # 3h
+  }
+
+
+  monitoring::check { "legacy puppet check":
+    ensure   => absent,
+    codename => "check_puppet",
+  }
+
+  file {
+    ["/usr/lib/nagios/plugins/contrib/check_puppet.pl",
+     "/usr/lib64/nagios/plugins/contrib/check_puppet.pl"]: ensure => absent }
+}
