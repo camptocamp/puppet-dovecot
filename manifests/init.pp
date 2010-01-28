67edc8a70d65a5a2d993848b4679c04d4f751771
Marc Fournier <marc.fournier@camptocamp.com>
2010-01-28 11:10:13 +0100

    pacekamer: new helper class for drbd.

--- manifests/init.pp
+++ manifests/init.pp
@@ -191,6 +191,30 @@ class pacemaker::apache::ssl {
 
 /*
 
+== Class: pacemaker::drbd
+
+Helper which includes the drbd::base class, but with service management
+disabled.  This is useful if DRBD needs to be managed by heartbeat, but you
+still want to benefit from the facilities provided in the drbd module.
+
+Requires:
+- drbd's puppet module
+
+Example usage:
+  include pacemaker::drbd
+
+*/
+class pacemaker::drbd inherits drbd::base {
+
+  Service["drbd"] {
+    ensure => undef,
+    enable => false,
+  }
+}
+
+
+/*
+
 == Definition: pacemaker::iptables
 
 A helper which allows setting iptables rules for pacemaker.
