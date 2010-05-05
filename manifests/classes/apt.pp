e0ada8de6741e5dda8f5685ede98dfd6962fa35e
Marc Fournier <marc.fournier@camptocamp.com>
2010-05-05 19:21:46 +0200

    apt: remove sources.list/preferences not managed by puppet.

--- manifests/classes/apt.pp
+++ manifests/classes/apt.pp
@@ -3,6 +3,16 @@ class apt {
     require => Exec["apt-get_update"]
   }
 
+  # ensure only files managed by puppet be present in this directory.
+  file { "/etc/apt/sources.list.d":
+    ensure  => directory,
+    source  => "puppet:///apt/empty/",
+    recurse => true,
+    purge   => true,
+    force   => true,
+    ignore  => ".placeholder",
+  }
+
   apt::conf {"10periodic":
     ensure => present,
     source => "puppet:///apt/10periodic",
--- manifests/definitions/preferences.pp
+++ manifests/definitions/preferences.pp
@@ -7,6 +7,7 @@ define apt::preferences($ensure="present", $package="", $pin, $priority) {
 
   common::concatfilepart { $name:
     ensure  => $ensure,
+    manage  => true,
     file    => "/etc/apt/preferences",
     content => template("apt/preferences.erb"),
     before  => Exec["apt-get_update"],
