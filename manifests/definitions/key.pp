65579e7980af2f2509b2acac3a46c5b81e56a0ee
Marc Fournier <marc.fournier@camptocamp.com>
2010-05-05 19:25:32 +0200

    apt::key -  parameter becomes optional, default is now to import key from a keyserver.

--- manifests/definitions/key.pp
+++ manifests/definitions/key.pp
@@ -1,7 +1,13 @@
-define apt::key($ensure = present, $source) {
+define apt::key($ensure=present, $source="") {
+
   case $ensure {
+
     present: {
-      exec { "/usr/bin/wget -O - '$source' | /usr/bin/apt-key add -":
+      exec { "import gpg key $name":
+        command => $source ? {
+          ""      => "gpg --keyserver pgp.mit.edu --recv-key '$name' && gpg --export --armor '$name' | /usr/bin/apt-key add -",
+          default => "wget -O - '$source' | /usr/bin/apt-key add -",
+        },
         unless => "apt-key list | grep -Fqe '${name}'",
         path   => "/bin:/usr/bin",
         before => Exec["apt-get_update"],
