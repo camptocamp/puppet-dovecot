bb6937d447fef3569f4c2f5eadac9aaadfc8ec5a
mbornoz <mathieu.bornoz@camptocamp.com>
2010-05-21 15:10:06 +0200

    added supported versions

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,13 +3,38 @@ import "definitions/*.pp"
   
 class postgresql {
   case $operatingsystem {
-    Debian:  { 
+    Debian: { 
       case $lsbdistcodename {
-        etch :  { include postgresql::debian::v8-3 }
-        lenny : { include postgresql::debian::v8-3 }
+        etch :   { include postgresql::debian::v8-3 }
+        lenny :  { include postgresql::debian::v8-3 }
         default: { fail "postgresql not available for ${operatingsystem}/${lsbdistcodename}"}
       }
     } 
+    default: { notice "Unsupported operatingsystem ${operatingsystem}" }
+  }
+}
+
+class postgresql::v8-3 {
+  case $operatingsystem {
+    Debian: {
+      case $lsbdistcodename {
+        etch :   { include postgresql::debian::v8-3 }
+        lenny :  { include postgresql::debian::v8-3 }
+        default: { fail "postgresql 8.3 not available for ${operatingsystem}/${lsbdistcodename}"}
+      }
+    }
+    default: { notice "Unsupported operatingsystem ${operatingsystem}" }
+  }
+}
+
+class postgresql::v8-4 {
+  case $operatingsystem {
+    Debian: {
+      case $lsbdistcodename {
+        lenny :  { include postgresql::debian::v8-4 }
+        default: { fail "postgresql 8.4 not available for ${operatingsystem}/${lsbdistcodename}"}
+      }
+    }
     default: { notice "Unsupported operatingsystem ${operatingsystem}" }
   }
 }
