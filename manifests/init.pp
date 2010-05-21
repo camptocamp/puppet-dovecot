8541c12af8cf0687ed1349baf61749f94fe9ff34
mbornoz <mathieu.bornoz@camptocamp.com>
2010-05-21 15:03:26 +0200

    added a default postgresql class

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,2 +1,15 @@
 import "classes/*.pp"
 import "definitions/*.pp"
+  
+class postgresql {
+  case $operatingsystem {
+    Debian:  { 
+      case $lsbdistcodename {
+        etch :  { include postgresql::debian::v8-3 }
+        lenny : { include postgresql::debian::v8-3 }
+        default: { fail "postgresql not available for ${operatingsystem}/${lsbdistcodename}"}
+      }
+    } 
+    default: { notice "Unsupported operatingsystem ${operatingsystem}" }
+  }
+}
