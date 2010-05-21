b9eae69436cbbd2a4a74b8f89a35bb657e99a384
root <root@c2cpc9.camptocamp.com>
2010-05-21 14:38:45 +0200

    Replaced standard users with c2c users for sysadmins.

--- manifests/app-tmpl/app-c2c-puppetmaster.pp
+++ manifests/app-tmpl/app-c2c-puppetmaster.pp
@@ -10,40 +10,8 @@ class app-c2c-puppetmaster {
   include puppet::master::passenger
   include mysql::server
 
-
-  user {
-    "mbornoz":
-      ensure      => present,
-      managehome  => true,
-      shell       => "/bin/bash",
-      comment     => "Mathieu Bornoz";
-    "marc":
-      ensure      => present,
-      managehome  => true,
-      shell       => "/bin/bash",
-      comment     => "Marc Fournier";
-    "cjeanneret":
-      ensure      => present,
-      managehome  => true,
-      shell       => "/bin/bash",
-      comment     => "Cedric Jeanneret";
-    "jbaubort":
-      ensure      => present,
-      managehome  => true,
-      shell       => "/bin/bash",
-      comment     => "Jean-Baptiste Aubort";
-    "ckaenzig":
-      ensure      => present,
-      managehome  => true,
-      shell       => "/bin/bash",
-      comment     => "Christian Kaenzig";
-    "mremy":
-      ensure      => present,
-      managehome  => true,
-      shell       => "/bin/bash",
-      comment     => "Marc Remy";
+  c2c::sshuser::sadb {["ckaenzig", "marc", "mbornoz", "cjeanneret", "jbaubort", "mremy"]:
+    groups  => "sysadmin",
   }
 
-  
-  
 }
