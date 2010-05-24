1135558a6d74a98aff65859c08355b68bef4d83f
mbornoz <mathieu.bornoz@camptocamp.com>
2010-05-24 16:12:58 +0200

    added the notify pg_createcluster

--- manifests/classes/postgresql-debian-base.pp
+++ manifests/classes/postgresql-debian-base.pp
@@ -6,4 +6,10 @@ This class is dedicated to the common parts
 shared by the different flavors of Debian
 
 */
-class postgresql::debian::base inherits postgresql::base {}
+class postgresql::debian::base inherits postgresql::base {
+  
+  Package["postgresql"] {
+    notify => Exec["pg_createcluster in utf8"],
+  }
+  
+}
