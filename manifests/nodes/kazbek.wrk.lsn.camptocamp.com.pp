0bcc6f929e88e1fb5a67ea7909bfc936145e73bb
Mathieu Bornoz <mbornoz@c2cpc9.camptocamp.com>
2010-06-08 17:08:14 +0200

    initial commit

---
+++ manifests/nodes/kazbek.wrk.lsn.camptocamp.com.pp
@@ -0,0 +1,5 @@
+node 'kazbek.wrk.lsn.camptocamp.com' {
+  $puppet_client_version = "0.25.4-2ubuntu6"
+  $default_ubuntu_user = "mbornoz"
+  include srv-workstation
+}
---
+++ manifests/srv-tmpl/srv-workstation.pp
@@ -0,0 +1,9 @@
+class srv-workstation {
+
+  ### Global attributes ##########################
+  $ps1label = "srv-workstation"
+
+  ### OS #########################################
+  include os-base
+
+}
