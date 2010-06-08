48072aa48311d40b662ab9958b47ee428f6d3a16
Mathieu Bornoz <mbornoz@c2cpc9.camptocamp.com>
2010-06-08 17:05:35 +0200

    not necessary with apt module

--- manifests/classes/ubuntu-lucid.pp
+++ manifests/classes/ubuntu-lucid.pp
@@ -4,16 +4,4 @@ class os::ubuntu-lucid inherits ubuntu {
     ensure => absent,
     before => Exec["apt-get_update"],
   }
-  apt::sources_list {"ubuntu-gutsy.sources":
-    ensure => absent,
-  }
-  apt::sources_list {"c2c-gutsy.sources":
-    ensure => absent,
-  }
-  apt::sources_list {"ubuntu-hardy.sources":
-    ensure => absent,
-  }
-  apt::sources_list {"c2c-hardy.sources":
-    ensure => absent,
-  }
 }
