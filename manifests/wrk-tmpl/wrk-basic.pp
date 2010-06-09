56206e362b3744a8a17ede542ee9dfad54a73639
Mathieu Bornoz <mathieu.bornoz@camptocamp.com>
2010-06-09 17:11:16 +0200

    fine-tuning

--- manifests/wrk-tmpl/wrk-basic.pp
+++ manifests/wrk-tmpl/wrk-basic.pp
@@ -1,11 +1,14 @@
-class srv-workstation {
+class wrk-basic {
 
   ### Global attributes ##########################
-  $ps1label = "srv-workstation"
+  $ps1label = "wrk-basic"
 
   ### OS #########################################
   include os-base
 
   ### MW #########################################
   include mw-java-v6
+  include bazaar::client
+  include apt::unattended-upgrade::automatic
+
 }
