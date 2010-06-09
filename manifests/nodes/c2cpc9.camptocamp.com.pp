cbeb2f7b3fbf1b22030075a03eb2441fe7851b04
Mathieu Bornoz <mathieu.bornoz@camptocamp.com>
2010-06-09 17:10:50 +0200

    set root_mail_recipient

--- manifests/nodes/c2cpc9.camptocamp.com.pp
+++ manifests/nodes/c2cpc9.camptocamp.com.pp
@@ -1,4 +1,5 @@
 node 'c2cpc9.camptocamp.com' {
+  $root_mail_recipient = "c2c.sysadmin@camptocamp.com"
   $postfix_relayhost = "mail.camptocamp.com"
   include postfix::satellite
   include srv-c2c-puppetmaster
