a3f59507f55017cfe3e51fcbceb2c808e17ec8f0
Mathieu Bornoz <mathieu.bornoz@camptocamp.com>
2010-06-09 17:12:12 +0200

    test default workstation manifest-template

--- manifests/nodes/kazbek.wrk.lsn.camptocamp.com.pp
+++ manifests/nodes/kazbek.wrk.lsn.camptocamp.com.pp
@@ -1,5 +1,5 @@
 node 'kazbek.wrk.lsn.camptocamp.com' {
   $puppet_client_version = "0.25.4-2ubuntu6"
-  $default_ubuntu_user = "mbornoz"
-  include srv-workstation
+  $c2c_workstation_users = ["mbornoz","marc","ckaenzig"]
+  include wrk-c2c
 }
