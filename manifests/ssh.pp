d48889d01a562f2525bfbf5ad2c7f8d0967057d0
Mathieu Bornoz <mbornoz@c2cpc9.camptocamp.com>
2010-06-09 09:59:47 +0200

    clean-up

--- manifests/ssh.pp
+++ manifests/ssh.pp
@@ -1,4 +1,4 @@
-class ssh-ng {
+class ssh {
   package { "ssh":
     ensure => installed
   }
@@ -8,16 +8,4 @@ class ssh-ng {
     hasrestart => true,
     pattern => "/usr/sbin/sshd",
   }
-  
-  # SSH known_hosts generation using puppet black magic (known as exported ressources)
-  #@@sshkey { $fqdn:
-  #  type => dsa,
-  #  key => $sshdsakey
-  #}
-#
-#  Sshkey <<| |>>
-#
-#  Sshkey {
-#    require => Package["ssh"]
-#  }
 }
