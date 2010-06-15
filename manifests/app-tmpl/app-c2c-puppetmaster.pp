437d552c46ef0d4c3d3226dd77534c8de08837b4
Cédric Jeanneret <cjeanneret@c2cpc9.camptocamp.com>
2010-06-15 13:56:47 +0200

    added puppet-dashboard configuration
    missing for now : "official" package - was installed via dpkg -i

--- manifests/app-tmpl/app-c2c-puppetmaster.pp
+++ manifests/app-tmpl/app-c2c-puppetmaster.pp
@@ -14,6 +14,10 @@ class app-c2c-puppetmaster {
     ensure => present;
   }
 
+  os::backported_package {"rake":
+    ensure => latest,
+  }
+
   c2c::sshuser::sadb {["ckaenzig", "marc", "mbornoz", "cjeanneret", "jbaubort", "mremy"]:
     groups  => "sysadmin",
   }
@@ -24,6 +28,30 @@ class app-c2c-puppetmaster {
     content => "## This part is managed by puppet
 %sysadmin ALL=(ALL) ALL
 ##\n",
+  }
+
+  apache::vhost-ssl {"pm.camptocamp.net":
+    ensure    => present,
+    cert      => "/var/lib/puppet/ssl/certs/pm.camptocamp.net.pem",
+    certkey   => "/var/lib/puppet/ssl/private_keys/pm.camptocamp.net.pem",
+    cacert    => "/var/lib/puppet/ssl/ca/ca_crt.pem",
+    certchain => "/var/lib/puppet/ssl/ca/ca_crt.pem",
+  }
+
+  apache::directive {"dashboard-local":
+    ensure    => present,
+    vhost     => "pm.camptocamp.net",
+    directive => "
+PassengerStatThrottleRate 120
+RackAutoDetect On
+RailsAutoDetect On
+
+RewriteEngine On
+RewriteCond %{HTTPS} ^off$
+RewriteCond %{REMOTE_ADDR} !127.0.0.1
+RewriteCond %{REMOTE_ADDR} !${ipaddress_eth0}
+rewriteRule ^/.* - [F,L]
+",
   }
 
 }
