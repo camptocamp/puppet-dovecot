bd0f428e77842f13c5643084e3b98b5339c208cd
Mathieu Bornoz <mathieu.bornoz@camptocamp.com>
2010-06-17 16:14:58 +0200

    fix bugs

--- files/usr/local/bin/get-sadb-users.rb
+++ files/usr/local/bin/get-sadb-users.rb
@@ -6,25 +6,28 @@ begin
   puts "#"
   puts "# This puppet recipe is generated with the script get-c2c-sadb-users.rb"
   puts "#"
-  puts "define app::c2c::sadb::users ($ensure=present, $groups=false) { {\n"
+  puts "define app::c2c::sadb::users ($ensure=present, $groups=false) {"
   open('http://sadb.camptocamp.com/user/internal/') {|f|
     f.each_line {|line| 
       args = line.split(";")
-      if args[1] != 'uid' and ["ssh-rsa","ssh-dss"].include?(args[6])
-        printf("
-  c2c::sshuser {'%s': 
-    ensure  => $present, 
-    uid     => %s, 
-    comment => '%s %s', 
-    email   => '%s', 
-    type    => '%s',
-    key     => '%s',
+      uid = args[1]
+      if uid != 'uid' and ["ssh-rsa","ssh-dss"].include?(args[6])
+        print <<EOF
+
+  c2c::sshuser {\"#{uid}\": 
+    ensure  => $ensure, 
+    uid     => url_get(\"${sadb}/user/#{uid}/uid_number\"),
+    comment => sprintf(\"%s %s\", url_get(\"${sadb}/user/mbornoz/firstname\"), url_get(\"${sadb}/user/mbornoz/lastname\")),
+    email   => url_get(\"${sadb}/user/#{uid}/email\"), 
+    type    => url_get(\"${sadb}/user/#{uid}/ssh_pub_key_type\"),
+    key     => url_get(\"${sadb}/user/#{uid}/ssh_pub_key\"),
     groups  => $groups, 
-    shell   => /bin/bash 
-  }\n",  args[1], args[5], args[2], args[3], args[4], args[6], args[7])
-      end
+  }
+EOF
+     end
     }
   }
+  puts "}"
 rescue OpenURI::HTTPError => error
   fail "Fetching URL #{url} failed with status #{error.message}"
 end
