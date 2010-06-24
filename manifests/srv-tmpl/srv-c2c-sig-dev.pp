ac0b2179cd2fe8767dc5a4ab9cb0f39cca5310b0
Mathieu Bornoz <mathieu.bornoz@camptocamp.com>
2010-06-24 18:06:03 +0200

    fine-tuning

---
+++ manifests/srv-tmpl/srv-c2c-sig-dev.pp
@@ -0,0 +1,27 @@
+class srv-c2c-sig-dev {
+
+  ### Global attributes ##########################
+  $server_group = "development"
+  $ps1label = "development"
+  $sudo_apache_admin_user = "deploy, %sigdev"
+  $sudo_postgresql_admin_user = "deploy, %sigdev"
+  $sudo_tomcat_admin_user = "deploy, %sigdev"
+
+  ### OS #########################################
+  include os-base
+  include os-server
+
+  ### MW #########################################
+  include mw-sig
+  include mw-apache
+  include mw-tomcat
+  include mw-postgis-8-4
+  include mw-postgresql-8-4
+
+  ### APP (generic) ##############################
+  include app-c2c-sig
+
+  ### APP (specific to this server) ##############
+  app::c2c::sadb::users{["all c2c users"]: groups => ["sigdev"]}
+
+}
