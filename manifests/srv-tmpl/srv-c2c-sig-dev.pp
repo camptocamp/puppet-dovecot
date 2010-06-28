aaa14693786e685c96f3373380e388904536d308
Mathieu Bornoz <mathieu.bornoz@camptocamp.com>
2010-06-28 13:32:55 +0200

    added a backward compatibility parameter for postgresql

--- manifests/srv-tmpl/srv-c2c-sig-dev.pp
+++ manifests/srv-tmpl/srv-c2c-sig-dev.pp
@@ -15,15 +15,22 @@ class srv-c2c-sig-dev {
   include mw-sig
   include mw-apache
   include mw-tomcat
-  include mw-postgis-8-4
-  include mw-postgresql-8-4
+  
+  # backward compatibility
+  if $postgresql_version == "8.3" {
+    include mw-postgis-8-3
+    include mw-postgresql-8-3
+  } else {
+    include mw-postgis-8-4
+    include mw-postgresql-8-4
+  }
 
   ### APP (generic) ##############################
   include app-c2c-sig
-  include app-c2c-sig-dev
   include app-c2c-remove-ldap-support
 
   ### APP (specific to this server) ##############
+  include app-c2c-sig-dev
   app::c2c::sadb::users{["all c2c users"]: groups => ["sigdev", "www-data"]}
 
 }
