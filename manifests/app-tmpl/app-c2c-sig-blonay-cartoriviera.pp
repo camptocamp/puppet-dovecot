1acb56b10b6f1009ad9cd9910b7433c537ffdeff
Mathieu Bornoz <mathieu.bornoz@camptocamp.com>
2010-06-28 14:57:21 +0200

    vhost and tomcat instance

--- manifests/app-tmpl/app-c2c-sig-blonay-cartoriviera.pp
+++ manifests/app-tmpl/app-c2c-sig-blonay-cartoriviera.pp
@@ -1,4 +1,17 @@
 class app-c2c-sig-blonay-cartoriviera {
+  
+  apache::vhost {"blonay-cartoriviera":
+    ensure  => present,
+    group   => sigdev,
+    mode    => 2775,
+    aliases => [$fqdn, server_alias_from_domain($fqdn)],
+  }
+
+  tomcat::instance {"tomcat1":
+    ensure => present,
+    group  => sigdev,
+  } 
+
   user {"admin":
     ensure     => present,
     managehome => true,
