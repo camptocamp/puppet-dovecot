e937f4391087607c2d3ff22aeebdaea3f2820f05
Mathieu Bornoz <mathieu.bornoz@camptocamp.com>
2010-06-28 15:50:57 +0200

    test

--- manifests/nodes/c2cpc21.camptocamp.com.pp
+++ manifests/nodes/c2cpc21.camptocamp.com.pp
@@ -1,3 +1,5 @@
-node "c2cpc21.camptocamp.com" {
+node 'c2cpc21.camptocamp.com' {
+  $aa = server_alias_from_domain($fqdn)
+  notify {"The value is: ${aa}": }
   include srv-c2c-sig-blonay-cartoriviera
 }
