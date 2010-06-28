4fee7b9faef421109fef87f5bc314b047dafdc47
Mathieu Bornoz <mathieu.bornoz@camptocamp.com>
2010-06-28 14:49:53 +0200

    migration from sa.camptocamp.com

---
+++ manifests/nodes/uabeea-geobi-demo.dmz.lsn.camptocamp.com.pp
@@ -0,0 +1,4 @@
+node 'uabeea-geobi-demo.dmz.lsn.camptocamp.com' {
+  $postgresql_version = "8.3"
+  include srv-c2c-sig-demo
+}
---
+++ manifests/nodes/uabeea-geobi-dev.int.lsn.camptocamp.com.pp
@@ -0,0 +1,4 @@
+node 'uabeea-geobi-dev.int.lsn.camptocamp.com' {
+  $postgresql_version = "8.3"
+  include srv-c2c-sig-dev
+}
