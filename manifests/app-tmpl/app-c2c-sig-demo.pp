0b069d329ebdadfea00090193a40f4e879cb0412
Mathieu Bornoz <mathieu.bornoz@camptocamp.com>
2010-06-28 13:47:55 +0200

    new demo/sig templates

---
+++ manifests/app-tmpl/app-c2c-sig-demo.pp
@@ -0,0 +1,20 @@
+class app-c2c-sig-demo {
+  
+  $project_name = $apache_vhost_name ? {
+    "" => project_name_from_domain($fqdn),
+    default => $fqdn,
+  }
+
+  apache::vhost {$project_name:
+    ensure  => present,
+    group   => sigdev,
+    mode    => 2775,
+    aliases => [$fqdn, server_alias_from_domain($fqdn)],
+  }
+
+  tomcat::instance {"tomcat1":
+    ensure => present,
+    group  => sigdev,
+  } 
+
+}
