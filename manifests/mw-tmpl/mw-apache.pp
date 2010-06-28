d13771d076ed9c9050dd25cb537452ce76f5d458
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2010-06-28 13:47:36 +0200

    now uses monitoring module

--- manifests/mw-tmpl/mw-apache.pp
+++ manifests/mw-tmpl/mw-apache.pp
@@ -5,11 +5,7 @@ class mw-apache {
   include apache::deflate
 
   if $server_group == "prod" {
-    nagios::service::distributed{"check_apachestatus!localhost!80":
-      ensure => present,
-      service_description => "apache2 status",
-      contact_groups => $nagios_contact_groups,
-    }
+    include monitoring::apache_status
   }
  
 }
