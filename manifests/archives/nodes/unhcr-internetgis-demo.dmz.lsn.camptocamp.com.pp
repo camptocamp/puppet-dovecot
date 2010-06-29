3907f6b9fe50abec7b4d0959d63a427bba3c003b
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2010-06-29 15:52:56 +0200

    migration pack

---
+++ manifests/archives/nodes/unhcr-internetgis-demo.dmz.lsn.camptocamp.com.pp
@@ -0,0 +1,3 @@
+node 'unhcr-internetgis-demo.dmz.lsn.camptocamp.com' {
+  include tmpl-sig-dev-ms5-0
+}
---
+++ manifests/nodes/formation1.wrk.lsn.camptocamp.com.pp
@@ -0,0 +1,4 @@
+node 'formation1.wrk.lsn.camptocamp.com' {
+  $c2c_workstation_users = ["alex"]
+  include wrk-c2c
+}
