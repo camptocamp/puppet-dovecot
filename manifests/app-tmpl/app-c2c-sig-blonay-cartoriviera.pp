0982464f1187437d511babb44225e897707f34b3
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2010-06-29 12:16:19 +0200

    added external user

--- manifests/app-tmpl/app-c2c-sig-blonay-cartoriviera.pp
+++ manifests/app-tmpl/app-c2c-sig-blonay-cartoriviera.pp
@@ -18,8 +18,18 @@ class app-c2c-sig-blonay-cartoriviera {
     home       => "/home/admin",
     shell      => "/bin/bash",
   }
-
   c2c::ssh_authorized_key{
     "alex on admin": sadb_user => "alex", user => "admin", require => User["admin"];
   }
+
+  c2c::sshuser {"ygillieron": 
+    ensure  => present, 
+    groups  => "admin", 
+    uid     => 2000, 
+    comment => "y.gillieron@b-t-i.ch", 
+    email   => "y.gillieron@b-t-i.ch",
+    type    => "rsa", 
+    key     => "AAAAB3NzaC1yc2EAAAABJQAAAIEAkc3duX7LBHnQndfFLgIVd8OCVkyNxBwz81MAO1TXkjVLi5TnkBngHe7w1oLOmygylVNd6Grag/iY9NX/6YilYTqRg4Q/RaYJBfG37o3ur4wnGYaHaAjyPTyT96RVvQ5DrVSaPlPWRCRgSxL6CUfrUx23mjP/JRCSAs55PxMwy+8=";
+  }
+
 }
