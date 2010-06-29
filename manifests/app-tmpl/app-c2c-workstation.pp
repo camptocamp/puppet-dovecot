671a63c9dfa24aec006e70b4c84e622e07b0bb25
Mathieu Bornoz <mathieu.bornoz@camptocamp.com>
2010-06-29 10:58:16 +0200

    added ssh pub key of backuppc

--- manifests/app-tmpl/app-c2c-workstation.pp
+++ manifests/app-tmpl/app-c2c-workstation.pp
@@ -15,4 +15,18 @@ class app-c2c-workstation {
     }
   }
 
+  # public ssh-key used by backuppc
+  ssh_authorized_key {"backuppc pubkey":
+    ensure  => present,
+    type    => "ssh-rsa",
+    key     => "AAAAB3NzaC1yc2EAAAABIwAAAQEAx312hsQ0ODFQlreB0WqdPF88XK6EOiDlqk92Emy/DGMcGqGsUxef9WW3CQOxx4qaPuf8da31IEJOEGvNT9fQhzABp5w579Ujzden3U+0dF4hGopioRwG0y2OgfE0QeOJAnrdQCeT0Ek/lkc7PcKdShmCxvAcVqGBTVixto26vJXdGmTPlZEfy09Lozpx874hDLg8zIYn8vjhrNrhjkxqyeVCFHfYxag+o3dgC1cphFDIbeb0D83OCnQYfz2mVAgmynypGd/o+T1B63NZGTaDrFMptWCrPEm3XO8H715Yr2mZbl5gxjIYrAGav0S6fY7SHClFzKagfY45RIDh+usZOw==",
+    user    => root,
+    options => [
+      "from=\"10.27.20.51,10.26.21.8,10.28.21.3\"",
+      "no-pty",
+      "no-port-forwarding",
+      "no-X11-forwarding",
+    ],
+  }
+  
 }
