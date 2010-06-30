ac9ecd2b0018dba3cfdbbb723ca7ea8d7502da66
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2010-06-30 08:39:21 +0200

    added new user and his ssh key

--- manifests/app-tmpl/app-c2c-sig-blonay-cartoriviera.pp
+++ manifests/app-tmpl/app-c2c-sig-blonay-cartoriviera.pp
@@ -39,4 +39,14 @@ class app-c2c-sig-blonay-cartoriviera {
     key     => "AAAAB3NzaC1yc2EAAAABJQAAAIEAkc3duX7LBHnQndfFLgIVd8OCVkyNxBwz81MAO1TXkjVLi5TnkBngHe7w1oLOmygylVNd6Grag/iY9NX/6YilYTqRg4Q/RaYJBfG37o3ur4wnGYaHaAjyPTyT96RVvQ5DrVSaPlPWRCRgSxL6CUfrUx23mjP/JRCSAs55PxMwy+8=";
   }
 
+  c2c::sshuser {"tcachin": 
+    ensure  => present, 
+    groups  => "admin", 
+    uid     => 2001, 
+    comment => "tcachin@blonay.ch", 
+    email   => "tcachin@blonay.ch",
+    type    => "rsa", 
+    key     => "AAAAB3NzaC1yc2EAAAABJQAAAIEAwtEQHB2J6f9y4RERcIZ7QcqBtUWsSgeGHAAOFfrU8t4FV/sNJnt5CAwpWXbDreVQfA8gjmrkeCkdEe5G2IVuqyN6mxTs4IeeWwIATvJA1NBOGXb79FONkiOh1B7hDfrkLdAACn1tykFiwPk+psFnFrdFUWA9fzEmJIJMXUYLKl0=";
+  }
+
 }
