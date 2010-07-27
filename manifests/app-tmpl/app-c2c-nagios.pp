69dd2ff1d3c2082590cb3c4ab7599d5044cbd6f4
Christian Kaenzig <christian.kaenzig@camptocamp.com>
2010-07-27 16:12:22 +0200

    New node, srv and app template for new nagios server.

---
+++ manifests/app-tmpl/app-c2c-nagios.pp
@@ -0,0 +1,78 @@
+# Class for specifics for srv-c2c-nagios
+#
+# Requires: 
+# Provides: 
+# Conflicts: 
+#
+
+class app-c2c-nagios {
+
+  include nagios::base
+  include nagios::nsca::server
+  include nagios::nrpe::client
+  include nagios::webinterface
+
+
+  nagios::local::hostgroup { [
+    "production"
+    
+    ]:
+  }
+
+
+  apache::vhost{$fqdn:
+    ensure => present,
+  }
+  apache::auth::htpasswd{"nagiosadmin":
+    vhost         => "$fqdn",
+    username      => "nagiosadmin",
+    cryptPassword => "/Rp7cgng51buI",
+    userFileName  => "nagios-htpasswd",
+  }
+
+  apache::redirectmatch {"slash-to-nagios3":
+    ensure => present,
+    vhost  => "$fqdn",
+    regex  => "^/$",
+    url    => "/nagios3/",
+  }
+
+  apache::directive {"nagios-config":
+    ensure    => present,
+    vhost     => "$fqdn",
+    directive => "# file managed by Puppet
+
+ScriptAlias /cgi-bin/nagios3     /usr/lib/cgi-bin/nagios3/
+ScriptAlias /nagios3/cgi-bin     /usr/lib/cgi-bin/nagios3/
+Alias       /nagios3/stylesheets /etc/nagios3/stylesheets
+Alias       /nagios3             /usr/share/nagios3/htdocs
+
+<DirectoryMatch (/usr/lib/cgi-bin/nagios3/|/usr/share/nagios3)>
+  Options FollowSymLinks ExecCGI Indexes
+  AllowOverride AuthConfig
+  Order Allow,Deny
+  Allow From All
+</DirectoryMatch>
+",
+  }
+
+  apache::directive{"nagios-access":
+    ensure    =>  present,
+    vhost     => "$fqdn",
+    directive => "# file managed by Puppet
+
+<Location />
+  Order deny,allow
+  Deny from all
+  Allow from 10.27.0.0/16
+  Allow from 128.179.66.0/24
+
+  AuthName \"Nagios Access\"
+  AuthType Basic
+  AuthUserFile /var/www/$fqdn/private/nagios-htpasswd
+  require valid-user
+</Location>
+",
+  }
+
+}
---
+++ manifests/nodes/c2cpc27.camptocamp.com.pp
@@ -0,0 +1,3 @@
+node 'c2cpc27.camptocamp.com' {
+  include srv-c2c-nagios
+}
