8b5c32870ff28fdd8eb13a3720757caa2f0b67a2
Marc Fournier <marc.fournier@camptocamp.com>
2010-07-29 10:35:12 +0200

    python: new class python::virtualenv

--- manifests/init.pp
+++ manifests/init.pp
@@ -45,6 +45,25 @@ class python::dev {
 }
 
 /*
+== Class: python::virtualenv
+
+Install virtualenv, to help people create python-"chroots" with packages not
+necessarily available in the distribution.
+
+Usage:
+  include python::virtualenv
+
+*/
+class python::virtualenv {
+
+  include python::dev
+
+  package { "python-virtualenv":
+    ensure => present,
+  }
+}
+
+/*
 
 == Class: python::mod_python
 
