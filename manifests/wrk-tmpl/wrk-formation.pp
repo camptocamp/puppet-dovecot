1503439a0eaeeef35018493f3ad7b71cab876418
mbornoz <mathieu.bornoz@camptocamp.com>
2010-08-11 14:51:02 +0200

    new formation templates

---
+++ manifests/wrk-tmpl/wrk-formation.pp
@@ -0,0 +1,27 @@
+class wrk-formation {
+
+  ### Global attributes ##########################
+  $ps1label = "wrk-formation"
+  $sudo_apache_admin_user = "%admin"
+  $sudo_postgresql_admin_user = "%admin"
+  $sudo_tomcat_admin_user = "%admin"
+
+  ### OS #########################################
+  include os-base
+  
+  ### MW #########################################
+  include mw-apache
+  include mw-java-v6
+  include mw-tomcat
+  include mw-postgresql-8-4
+  include mw-postgis-8-4
+
+  ### APP (generic) ##############################
+  include app-workstation-tuning
+  include app-workstation-packages
+
+  ### APP (specific to this server) ##############
+  include app-c2c-workstation-formation
+  include app-c2c-workstation-formation-sig
+
+}
