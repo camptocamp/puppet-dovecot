783e3c7b14a323e67564f2af0ddd81cfc1a852c1
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2010-08-17 09:52:25 +0200

    added "openvz" class which includes "server" part.
    also added some documentation.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,2 +1,12 @@
 import "classes/*.pp"
-#import "definitions/*.pp"
+import "definitions/*.pp"
+
+/*
+
+== Class openvz
+Install an openvz hardware node.
+
+*/
+class openvz {
+  include openvz::server
+}
