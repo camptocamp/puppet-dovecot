0db8a91cad4b0926a42285cbda456dda640837ae
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2010-08-18 10:36:38 +0200

    in fact admin GROUP should have the sudo rights

--- manifests/srv-tmpl/srv-kartenportal.pp
+++ manifests/srv-tmpl/srv-kartenportal.pp
@@ -2,7 +2,7 @@ class srv-kartenportal {
   ### Global attributes ##########################
   $server_group = "prod" #(one of dev, demo or prod)
   $ps1label = "kartenportal hosting"
-  $sudo_lighttpd_admin_user = "admin"
+  $sudo_lighttpd_admin_user = "%admin"
 
   ### OS #########################################
   include os-base
