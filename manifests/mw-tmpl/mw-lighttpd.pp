4d50cba4f63a1d8c1857c88f5a77944e71674e68
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2010-09-07 14:43:28 +0200

    midleware class for lighty
    Install lighty, and prevents apache to be installed if we want nagios on
    the host.

---
+++ manifests/mw-tmpl/mw-lighttpd.pp
@@ -0,0 +1,35 @@
+class mw-lighttpd {
+  include lighttpd::administration
+
+  if $server_group == "prod" {
+    # if we install nagios before lighty on a debian, it also installs
+    # apache2 - so lighty cannot start. UGLY from debian packagers -.-
+    service {"apache2":
+      ensure => stopped,
+      enable => false,
+      before => Package["lighttpd"],
+    }
+
+    class my-lighty inherits lighttpd::base {
+      case $operatingsystem {
+        /Debian|Ubuntu/: {
+          Package["lighttpd"] {
+            before => Class["nagios::debian::packages"],
+          }
+        }
+        /RedHat|CentOS/: {
+          Package["lighttpd"] {
+            before => Class["nagios::redhat::packages"],
+          }
+        }
+      }
+    }
+
+    include my-lighty
+    include monitoring::webserver
+
+  } else {
+    include lighttpd
+  }
+
+}
