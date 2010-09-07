744fe7634f287269f56cf2a1889fc18384716c07
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2010-09-07 14:45:26 +0200

    added missing require for users

--- manifests/app-tmpl/app-kartenportal.pp
+++ manifests/app-tmpl/app-kartenportal.pp
@@ -22,6 +22,7 @@ class app-kartenportal {
     key     => "AAAAB3NzaC1yc2EAAAABIwAAAIEAvuiJlmtzTqTTKHDVJGeWx3j6pVVebABd4gVmrk1dTS81xUG70Ml8Wb2r7TFoYsE+RRSIaId2eyv5OMqGxUpZ6ehvS0jWrkQfim0gkz4N/d9QOmQq/Rp4MUAROymhHQ7SUcj8ogPr6IBKtr5cm3l2yMSb3isKFR9TMJYaHAFb/0E=",
     groups  => "admin",
     uid     => 1001,
+    require => User["admin"],
   }
 
   c2c::sshuser {"keo":
@@ -32,5 +33,6 @@ class app-kartenportal {
     key     => "AAAAB3NzaC1yc2EAAAABIwAAAQEAwNh7DwFyVDUK1aCNHOnTVgCsBiDYSrokLHhDT5K9yTePGlHnEuH1EIOcuxnrlWTSjZG1epL7Sh6YxW6EfAemF2hDCbj4pgRRycw+RIvw1AzgofK2oVyVL8DJxFWqAofI5IW30EwkgPL9bKWbt47P1fsQDJsvnWjTQncoVGQ3x/ExEaA9HUC8HPKrvak7/OZWkD5gexRjgdLktD8VJcvgFdtb82BCc8Q8kZfQzvikmk9LZM6/XF4CRmg07fVXRqzEbHka9NBfR58G092C9ETHAcPZlnPptm1w1nwD1RDxqMFodjxLpfAVml9ud5UsA6ZUaxOL5vkM2nD8RREif6f/4Q==",
     groups  => "admin",
     uid     => 1002,
+    require => User["admin"],
   }
 }
