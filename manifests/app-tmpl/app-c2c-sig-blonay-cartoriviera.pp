9fdb846299304f2a41892e39ed37cc2c3d3c7987
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2010-09-10 13:27:29 +0200

    added new user - rt#132534

--- manifests/app-tmpl/app-c2c-sig-blonay-cartoriviera.pp
+++ manifests/app-tmpl/app-c2c-sig-blonay-cartoriviera.pp
@@ -89,4 +89,14 @@ class app-c2c-sig-blonay-cartoriviera {
     key     => "AAAAB3NzaC1yc2EAAAABJQAAAIEAzUykFAtryuOPkAwMc0aItNGQvfurykXngWqIhQLlDMwrV5w1a5EvfMRUiGqZOYMv/fugieXYDG391JsE9jtUI4S8JqqYI9XoL9b9z77Ml2lNYXLtpfpqsDmaYZj/npM5sR8Q0tt9X+ak8er/b2SZTuD+r4rMlT9L1b3WhqsVyyU=";
   }
 
+  c2c::sshuser {"vrizzi":
+    ensure  => present, 
+    groups  => "admin", 
+    uid     => 2007, 
+    comment => "v.rizzi@b-t-i.ch", 
+    email   => "v.rizzi@b-t-i.ch",
+    type    => "rsa", 
+    key     => "AAAAB3NzaC1yc2EAAAABJQAAAIEA1LquxRLS/K6zWZ4rFZaiMwHVnze4i4AsCv7FmApvwLPBMDVGkaiDpEq/alargiWEdS7ir2wMnbDq1GsKueOGtdeRhCugwBvwhuhZLY7I+daq6HceZWlFgrxxCe6mJ2lsyMVTJ85lk8a8utUe4MbxP8gRFbLI4N5LsDvQsoRtQTE=";
+  }
+
 }
