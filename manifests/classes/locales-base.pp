fff9958f6199d2abe2f9efc910673939c9384ee1
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2010-09-14 10:05:01 +0200

    empty class
    this class is only for the module structure.

--- manifests/classes/locales-base.pp
+++ manifests/classes/locales-base.pp
@@ -3,13 +3,7 @@
 ==Class: locales::base
 Creates shared resources
 
-Do NOT include this class - please refere to init.pp
 
 */
 class locales::base {
-  exec {"locale-gen":
-    refreshonly => true,
-    command => "locale-gen",
-    timeout => 30,
-  }
 }
