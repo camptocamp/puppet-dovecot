4430845d2f75c6a012c87c96ab4eda93e88487de
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2010-09-14 10:05:44 +0200

    empty class
    only for module structure. Should contain debian-only things, shared
    between locales::debian::source and locales::debian::all

--- manifests/classes/locales-debian.pp
+++ manifests/classes/locales-debian.pp
@@ -6,17 +6,4 @@ Debian specialities. Inherits from locales::base.
 
 */
 class locales::debian inherits locales::base{
-  package {"locales-all":
-    ensure => absent,
-  }
-
-  package {"locales":
-    ensure => present,
-    require => Package["locales-all"],
-  }
-
-  Exec["locale-gen"] {
-    require => Package["locales"],
-  }
-
 }
