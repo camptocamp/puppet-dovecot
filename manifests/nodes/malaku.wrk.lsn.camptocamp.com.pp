b29b97e402db0cda0875b129a1844ca12c27eda4
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2010-09-21 14:30:46 +0200

    given to fclementi

--- manifests/nodes/malaku.wrk.lsn.camptocamp.com.pp
+++ manifests/nodes/malaku.wrk.lsn.camptocamp.com.pp
@@ -1,4 +1,4 @@
 node 'malaku.wrk.lsn.camptocamp.com' {
-  $c2c_workstation_users = ["gbaconnier", "jbove"]
+  $c2c_workstation_users = ["fclementi", "jgrandguillaume", "vrenaville"]
   include wrk-c2c
 }
