34bd97b49f3a5a3b1051977112d02c4df617631a
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2010-09-23 11:05:57 +0200

    added centos support

--- manifests/classes/monitoring-params.pp
+++ manifests/classes/monitoring-params.pp
@@ -1,7 +1,7 @@
 class monitoring::params {
 
   $mainplugins = $operatingsystem ? {
-    RedHat => $architecture ? {
+    /RedHat|CentOS/ => $architecture ? {
       x86_64 => "/usr/lib64/nagios/plugins/",
       i386   => "/usr/lib/nagios/plugins/",
     },
