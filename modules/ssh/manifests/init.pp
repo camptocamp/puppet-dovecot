3d917d6053d7a7eba1fddbd8a1f0bcc3e7336051
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2010-09-30 08:59:13 +0200

    added centos/redhat support

--- modules/ssh/manifests/init.pp
+++ modules/ssh/manifests/init.pp
@@ -1,11 +1,22 @@
 class ssh {
-  package { "ssh":
-    ensure => installed
+  case $operatingsystem {
+    /RedHat|CentOS/: {
+      package { "openssh":
+        ensure => installed,
+        alias  => "ssh",
+      }
+    }
+    /Debian|Ubuntu/: {
+      package { "ssh":
+        ensure => installed
+      }
+    }
   }
 
   service { "ssh":
-    ensure => running,
+    ensure     => running,
     hasrestart => true,
-    pattern => "/usr/sbin/sshd",
+    pattern    => "/usr/sbin/sshd",
+    require    => Package["ssh"],
   }
 }
