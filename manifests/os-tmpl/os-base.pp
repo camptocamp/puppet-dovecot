217c3c50af7b67c9ebc729afe36f0e97f3c99d1e
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2010-10-01 09:33:53 +0200

    wrong key

--- manifests/os-tmpl/os-base.pp
+++ manifests/os-tmpl/os-base.pp
@@ -17,7 +17,7 @@ class os-base {
     user    => root,
     options => ['command="/usr/local/sbin/sajama-command-file.py"',"no-pty","no-port-forwarding","no-X11-forwarding"],
     type    => "ssh-rsa",
-    key     => "AAAB3NzaC1yc2EAAAABIwAAAQEAyygqhBCEp2kRbZgs/a1Vj72QGGiV7QgOi/yEB7Bo1Ek0eHoK35AGUtM/IwsdjRT41VjzcDx616qXlKlHCL/MKiZYqYZ1XH529BSQCXfRa+IF80u42pRgUORLUCrPqDc3VGmRZaciMNCC0Ko3IBgNj+diDPtApg/VDZYe1qkzURbajDipn+ESONWJW/S91BA0czyTMhIoxUTmIfK7+f/Al1DwaCy8AIFq63KivU+re7ruh4rIUgeWXd1C8CnTzwKjpQpNphOq0TCsK2kOddimaaj9Rhvy1lpABk0MvagXr6wG/vf9kYDsAmht8ROLMHj85IwauY6DuqCsDuTlMlw0vw==",
+    key     => "AAAAB3NzaC1yc2EAAAABIwAAAQEAyygqhBCEp2kRbZgs/a1Vj72QGGiV7QgOi/yEB7Bo1Ek0eHoK35AGUtM/IwsdjRT41VjzcDx616qXlKlHCL/MKiZYqYZ1XH529BSQCXfRa+IF80u42pRgUORLUCrPqDc3VGmRZaciMNCC0Ko3IBgNj+diDPtApg/VDZYe1qkzURbajDipn+ESONWJW/S91BA0czyTMhIoxUTmIfK7+f/Al1DwaCy8AIFq63KivU+re7ruh4rIUgeWXd1C8CnTzwKjpQpNphOq0TCsK2kOddimaaj9Rhvy1lpABk0MvagXr6wG/vf9kYDsAmht8ROLMHj85IwauY6DuqCsDuTlMlw0vw==",
   }
 
   file {"/usr/local/sbin/sajama-command-file.py":
