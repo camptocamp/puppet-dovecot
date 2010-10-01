dde0710d38d72f50ffb235729590579e45df7bfd
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2010-10-01 13:47:39 +0200

    new check: mdadm raid status

---
+++ modules/monitoring/manifests/classes/monitoring-raid-mdadm.pp
@@ -0,0 +1,14 @@
+class monitoring::raid::mdadm {
+  include monitoring::params
+  monitoring::check {"System: mdadm raid status":
+    ensure   => present,
+    codename => "check_mdadm",
+    command  => "check_linux_raid",
+    type     => "passive",
+    server   => $nagios_nsca_server,
+    package  => $operatingsystem? {
+      /RedHat|CentOS/ => "nagios-plugins-linux_raid",
+      default         => false,
+    }
+  }
+}
