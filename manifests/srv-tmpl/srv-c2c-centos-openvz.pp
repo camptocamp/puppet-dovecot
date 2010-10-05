95a97e48c260e89798007f52eec3d51b0858c17e
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2010-10-05 08:15:59 +0200

    added contactgroup

--- manifests/srv-tmpl/srv-c2c-centos-openvz.pp
+++ manifests/srv-tmpl/srv-c2c-centos-openvz.pp
@@ -2,6 +2,7 @@ class srv-c2c-centos-openvz {
   ### Global attributes ##########################
   $ps1label = "openvz HN"
   $server_group = "prod"
+  $basic_contact_group = "admins"
 
   ### OS #########################################
   include os-base
