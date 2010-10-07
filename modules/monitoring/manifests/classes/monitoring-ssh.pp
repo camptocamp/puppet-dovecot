d7f39fe5cbf4230c46393db3327ade416e12174a
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2010-10-07 09:41:37 +0200

    updated class

--- modules/monitoring/manifests/classes/monitoring-ssh.pp
+++ modules/monitoring/manifests/classes/monitoring-ssh.pp
@@ -1,23 +1,4 @@
 class monitoring::ssh {
-
-  monitoring::check { "Process: sshd":
-    codename => "check_sshd_process",
-    command  => "check_procs",
-    options  => "-p 1 -w 1: -c 1: -C sshd",
-    type     => "passive",
-    server   => $nagios_nsca_server,
-    package  => $operatingsystem ?{
-      /RedHat|CentOS/ => "nagios-plugins-procs",
-      default => false
-    }
-  }
-
-  # monitor ssh service from nagios server
-  monitoring::check { "SSH service":
-    codename => "check_ssh_service",
-    command  => "check_ssh",
-    options  => $fqdn,
-    type     => "remote",
-    server   => $nagios_nsca_server,
-  }
+  include monitoring::ssh::process
+  include monitoring::ssh::service
 }
