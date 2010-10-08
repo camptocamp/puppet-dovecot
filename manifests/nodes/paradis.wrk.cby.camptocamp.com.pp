1b5357235bbaa6a1d657c8f4e2bb4a52b068c715
Christian Kaenzig <christian.kaenzig@camptocamp.com>
2010-10-08 11:43:00 +0200

    Changé shell en zsh pour aabt sur sa workstation.

--- manifests/nodes/paradis.wrk.cby.camptocamp.com.pp
+++ manifests/nodes/paradis.wrk.cby.camptocamp.com.pp
@@ -1,4 +1,5 @@
 node 'paradis.wrk.cby.camptocamp.com' {
-  $c2c_workstation_users = ["aabt","pierre","elemoine","pmauduit","yjacolin","fjacon","bbinet","fvanderbiest"]
+  $c2c_workstation_users = ["pierre","elemoine","pmauduit","yjacolin","fjacon","bbinet","fvanderbiest"]
   include wrk-c2c
+  c2c::workstation::sadb::user{"aabt": shell => "/bin/zsh"}
 }
