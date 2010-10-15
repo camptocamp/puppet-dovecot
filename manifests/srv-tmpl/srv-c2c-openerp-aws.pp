48ec19f2d41f1ee0c41d776df0d1b65eca842b3b
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2010-10-15 10:35:32 +0200

    template for aws openerp instances

---
+++ manifests/srv-tmpl/srv-c2c-openerp-aws.pp
@@ -0,0 +1,24 @@
+# Server type used on node !insert node name here!
+#
+
+class srv-c2c-openerp-aws {
+
+  ### Global attributes ##########################
+  $server_group = "prod" #(one of dev, demo or prod)
+  $ps1label = "aws - OpenERP"
+  $sudo_apache_admin_user = 'openerp'
+  $sudo_postgresql_admin_user = 'openerp'
+  $openerp_project = 'openerp'
+  $is_external = true
+
+  ### OS #########################################
+  include os-base
+  include os-server
+
+  ### MW #########################################
+  include mw-postgresql-8-4
+  include mw-openerp
+
+  ### APP (generic) ##############################
+
+}
