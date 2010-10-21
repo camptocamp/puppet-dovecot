da3b6e04a99829fc8b5764f6339fffc46e95caef
mbornoz <mathieu.bornoz@camptocamp.com>
2010-10-21 11:00:03 +0200

    added a useful package

--- modules/buildenv/manifests/classes/deb.pp
+++ modules/buildenv/manifests/classes/deb.pp
@@ -1,7 +1,7 @@
 class buildenv::deb {
 
   package {
-    ["lintian", "dput", "dpatch", "devscripts", "build-essential", "dh-make", "dpkg-dev", "fakeroot"]:
+    ["lintian", "dput", "dpatch", "devscripts", "build-essential", "dh-make", "dpkg-dev", "fakeroot", "cdbs"]:
     ensure => present,
   }
 
