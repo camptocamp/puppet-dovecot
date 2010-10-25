fe66e444f429ac9dae3271d1d7fb963d43ea1da0
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2010-10-25 15:23:57 +0200

    new ve - prod-unhcr with lenny

---
+++ manifests/nodes/c2cpc54.camptocamp.com.pp
@@ -0,0 +1,3 @@
+node "c2cpc54.camptocamp.com" {
+  include srv-c2c-unhcr-prod
+}
---
+++ manifests/srv-tmpl/srv-c2c-unhcr-prod.pp
@@ -0,0 +1,26 @@
+class srv-c2c-unhcr-prod {
+
+  ### Global attributes ##########################
+  $server_group = "prod"
+  $ps1label = "unhcr production"
+  $sudo_apache_admin_user = "deploy, %admin"
+  $sudo_postgresql_admin_user = "deploy, %admin"
+  $sudo_tomcat_admin_user = "deploy, %admin"
+
+  ### OS #########################################
+  include os-base
+  include os-server
+
+  ### MW #########################################
+  include mw-sig
+  include mw-apache-ssl
+  include mw-tomcat
+  include mw-postgis-8-4
+  include mw-postgresql-8-4
+
+  ### APP (generic) ##############################
+  include app-c2c-sig
+
+  ### APP (specific to this server) ##############
+  include app-c2c-unhcr-dev
+}
