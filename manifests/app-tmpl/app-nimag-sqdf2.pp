4ce9bf90ec87e615dc252d279288d8d2a1ac4f07
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2010-11-02 09:25:43 +0100

    added new configurations
    - postfix configuration
    - ssh key for rdiff-backup and ldap-sync
    - mysql full access to osubilia

--- manifests/app-tmpl/app-nimag-sqdf2.pp
+++ manifests/app-tmpl/app-nimag-sqdf2.pp
@@ -3,7 +3,17 @@ class app-nimag-sqdf2 {
   include mysql::server
   include openldap::server::ssl
   include monitoring::ldap
+  include rdiff-backup::client
 
+  include postfix
+  postfix::virtual {"@sqdf2.vserver.nimag.net":
+    ensure => present,
+    destination => "sqdf.sysadmin@camptocamp.com",
+  }
+
+  postfix::hash { "/etc/postfix/virtual":
+    ensure => present,
+  }
 
   file {"/etc/ldap/config.ldif":
     ensure  => present,
@@ -11,6 +21,24 @@ class app-nimag-sqdf2 {
     group   => root,
     mode    => 0644,
     source  => "puppet:///modules/avocatsch/ldap/config.ldif",
+    require => Class["openldap::server::ssl"],
+  }
+
+  file {"/etc/ldap/slapd.conf.old":
+    ensure  => present,
+    owner   => root,
+    group   => root,
+    mode    => 0644,
+    source  => "puppet:///modules/avocatsch/ldap/slapd.conf.old",
+    require => Class["openldap::server::ssl"],
+  }
+
+  file {"/etc/ldap/schema/authldap.schema":
+    ensure  => present,
+    owner   => root,
+    group   => root,
+    mode    => 0644,
+    source  => "puppet:///modules/avocatsch/ldap/authldap.schema",
     require => Class["openldap::server::ssl"],
   }
   
@@ -42,6 +70,30 @@ class app-nimag-sqdf2 {
     source  => "puppet:///modules/avocatsch/ldap/DB_CONFIG",
     require => Class["openldap::server::ssl"],
   }
+
+  file {"/usr/local/sbin/ldap-sync.sh":
+    ensure => present,
+    owner  => root,
+    group  => root,
+    mode   => 0755,
+    source => "puppet:///modules/avocatsch/ldap/ldap-sync.sh",
+  }
+
+  ssh_authorized_key {"rdiff-backup on backup-sqdf":
+    ensure  => present,
+    user    => root,
+    type    => 'ssh-rsa',
+    options => ['command="/usr/bin/rdiff-backup --server"','no-pty','no-port-forwarding','no-X11-forwarding'],
+    key     => "AAAAB3NzaC1yc2EAAAABIwAAAIEAz2Pd5Sn8AzrHV16hIGgdaCtjUt/ql9ob1rMSqvN9pP5dvs89wGWx8IxygqCpONYyn8QxDNYp7M8Daj9R+Y3o2dbWOxOHV0HZKYq+SJ8qGtO6If0b++J5FGBrOaorL+i5XdWlfurjQ//w9l9CZnkX6qqel6Pezrv/GMQ8xPOTMz8=",
+  }
+
+  ssh_authorized_key {"root on artemis - ldap-sync":
+    ensure  => present,
+    user    => root,
+    type    => 'ssh-dss',
+    options => ['no-port-forwarding','no-X11-forwarding'],
+    key     => "AAAAB3NzaC1kc3MAAACBANqJszXYn3lufHhJRL+8iIndQxX9Eb+o+ODQTyu7nD2gxQvHfQxoXZnBUa8sbR/Hg56o+gbf6xMZKj5fPyVB3SavDKhB78rkMcjxQk4BCQINtsPTb9QLPNPrLfCgHtqOxqm/D3LcueohbtgMhtg1lyKXLASurDjG36TCqcXf3CjXAAAAFQDC94U4f9YZPx/JFbptEYt5gM3bZwAAAIAp5PrWkbWBSZ/3ScCQIYLok+21dX+maU4Lfzo6TvdUnoGnGQsnhjs+/zjFGxByfz72dUjSCTfCJpxWdThgQKXEPpQJ4AZnQcN107oYsWxPdhDmJbinNXe1FHjNiCrm6ATZob3RJBVDOjdzraOngymfXBvXZ0fQV4NSAC0Wjv/WbQAAAIEAmVbauTtktcJk6k1iOI/OhaIThdLcw86NNNcoLLpSGrhY+SyJtQY4gFnxCHQuBV5dZmMmT+39ZexUXwAH7d2m0fVT8XB9XLdZeqUujaVRk2vDAIxYAmL/nGFPj4Z0df/RoRW5oDGHwC0qwwUezBhJYldIoC1pGceEPUf7Wb3j0+Q=",
+  }
   
   augeas {"configure slapd defaults":
     context => "/files/etc/default/slapd/",
@@ -64,6 +116,15 @@ class app-nimag-sqdf2 {
     home   => "/home/osubilia",
     groups => "www-data",
     managehome => true,
+  }
+
+  file {"/home/osubilia/.my.cnf":
+    ensure  => present,
+    owner   => osubilia,
+    group   => osubilia,
+    mode    => 0600,
+    source  => "file:///root/.my.cnf",
+    require => User["osubilia"],
   }
 
   ssh_authorized_key {"olivier.subilia@avocats-ch.ch":
