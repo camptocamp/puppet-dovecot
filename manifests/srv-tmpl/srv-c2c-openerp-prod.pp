35bc3b3c111cb3148622d89fd6e70f4028d73b53
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2010-11-02 15:53:31 +0100

    added missing file. oops

---
+++ manifests/srv-tmpl/srv-c2c-openerp-prod.pp
@@ -0,0 +1,18 @@
+class srv-c2c-openerp-prod {
+
+  ### Global attributes ##########################
+  $server_group = "prod" #(one of dev, demo or prod)
+  $sudo_apache_admin_user = 'openerp'
+  $sudo_postgresql_admin_user = 'openerp'
+
+  ### OS #########################################
+  include os-base
+  include os-server
+
+  ### MW #########################################
+  include mw-postgresql-8-4
+  include mw-openerp
+
+  ### APP (generic) ##############################
+
+}
