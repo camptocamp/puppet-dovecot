92d6a782af94959e1686d5ff8b99f0053018ad91
Christian Kaenzig <christian.kaenzig@camptocamp.com>
2010-11-02 17:58:22 +0100

    c2c: fixed update-repository-list.py script.

--- modules/c2c/files/usr/local/bin/update-repository-list.py
+++ modules/c2c/files/usr/local/bin/update-repository-list.py
@@ -15,17 +15,18 @@ import os, sys, gzip, time
 packages = {}
 for repo in repos:
   r_path = "/var/packages/" + repo
-  packages[repo] = {}
 
   distribs = [x for x in os.listdir(r_path + "/dists/") if os.path.isdir(r_path + "/dists/" + x)]
   for distrib in distribs:
     d_path = r_path + "/dists/" + distrib
-    packages[repo][distrib] = {}
+    if distrib not in packages:
+      packages[distrib] = {}
 
     components = [x for x in os.listdir(d_path) if os.path.isdir(d_path + "/" + x)]
     for component in components:
       c_path = d_path + "/" + component
-      packages[repo][distrib][component] = {}
+      if component not in packages[distrib]:
+        packages[distrib][component] = {}
 
       archs = [x.split("-",1)[1] for x in os.listdir(c_path) if x.startswith("binary-") and os.path.isdir(c_path + "/" + x)]
       for arch in archs:
@@ -35,24 +36,22 @@ for repo in repos:
         pname = pversion = None
 	for line in packages_data.split("\n"):
           if not line and pname is not None:
-	    if pname in packages[repo][distrib][component]:
-	      packages[repo][distrib][component][pname]["arch"].append(arch)
+	    if pname in packages[distrib][component]:
+	      packages[distrib][component][pname]["versions"][repo] = pversion
+	      packages[distrib][component][pname]["archs"][repo] = arch
 	    else:
-	      packages[repo][distrib][component][pname] = {"version": pversion, "arch": [arch]}
+	      packages[distrib][component][pname] = {"versions": {repo: pversion}, "archs": {repo: arch}}
 	  elif line.startswith("Package:"):
 	    pname = line.split(" ",1)[1]
           elif line.startswith("Version:"):
 	    pversion = line.split(" ",1)[1]
 
 
-      if not packages[repo][distrib][component]:
-	del packages[repo][distrib][component]
+      if not packages[distrib][component]:
+	del packages[distrib][component]
 
-    if not packages[repo][distrib]:
-      del packages[repo][distrib]
-
-  if not packages[repo]:
-    del packages[repo]
+    if not packages[distrib]:
+      del packages[distrib]
 
 
 # Create HTML
@@ -61,37 +60,35 @@ html = """<html>
 <title>Camptocamp Debian/Ubuntu package repositories</title>
 <style  TYPE="text/css">
 body { font-family:sans-serif; }
-div.repo { background-color: #afa; padding: 10px; margin: 10px; }
-div.distrib { background-color: #faa; padding: 10px; margin: 10px; }
+div.distrib { background-color: #aaf; padding: 10px; margin: 10px; }
 div.component { background-color: #fff; padding: 10px; margin: 10px; }
 th { text-align: left; }
-td.p_name { width: 15em; }
-td.p_version { width: 15em; }
+th.p_name { width: 15em; }
+th.p_version { width: 15em; }
 </style>
 </head>
 <body>
 <p>Repository pkg.camptocamp.net - updated on %s
 """ %(time.strftime("%Y-%m-%d %H:%M:%S"))
-for repo in repos:
-  html += '<div class="repo"><h1>%s</h1>\n' %(repo)
+for distrib in packages.keys():
+  html += '<div class="distrib"><h2>%s</h2>\n' %(distrib)
 
-  for distrib in packages[repo].keys():
-    html += '<div class="distrib"><h2>%s</h2>\n' %(distrib)
+  for component in packages[distrib].keys():
+    html += '<div class="component"><h3>%s</h3>\n' %(component)
 
-    for component in packages[repo][distrib].keys():
-      html += '<div class="component"><h3>%s</h3>\n' %(component)
+    html += '<table><tr><th class="p_name">Package</th><th class="p_version">Stable</th><th class="p_version">Staging</td><th class="p_version">Dev</td></tr>\n'
 
-      html += '<table><tr><th class="p_name">Package</th><th class="p_version">Version</th><th class="p_archs">Archs</th></tr>\n'
+    packages_names = packages[distrib][component].keys()
+    packages_names.sort()
+    for package in packages_names:
+      versions = packages[distrib][component][package]["versions"]
+      for repo in repos:
+        if repo not in versions:
+          versions[repo] = "-"
+      html += '<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>\n' %(package, versions["stable"], versions["staging"], versions["dev"])
 
-      packages_names = packages[repo][distrib][component].keys()
-      packages_names.sort()
-      for package in packages_names:
-        infos = packages[repo][distrib][component][package]
-	html += '<tr><td class="p_name">%s</td><td class="p_version">%s</td><td class="p_archs">%s</td></tr>\n' %(package, infos["version"], " ".join(infos["arch"]))
+    html += '</table>'
 
-      html += '</table>'
-
-      html += '</div>'
     html += '</div>'
   html += '</div>'
 
