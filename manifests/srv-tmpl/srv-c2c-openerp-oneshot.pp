8281e175271f9c95bd62477fc1a4508bbbef01ff
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2010-11-02 07:20:31 +0100

    we have to define $openerp_project and $ps1label in node file

--- manifests/srv-tmpl/srv-c2c-openerp-oneshot.pp
+++ manifests/srv-tmpl/srv-c2c-openerp-oneshot.pp
@@ -5,10 +5,8 @@ class srv-c2c-openerp-oneshot {
 
   ### Global attributes ##########################
   $server_group = "demo" #(one of dev, demo or prod)
-  $ps1label = "debonix erp"
   $sudo_apache_admin_user = 'openerp'
   $sudo_postgresql_admin_user = 'openerp'
-  $openerp_project = 'debonix'
 
   ### OS #########################################
   include os-base
