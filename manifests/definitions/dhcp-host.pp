086e580fed2f32d5a63adcc70763ca4c189499b6
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2010-11-05 08:35:57 +0100

    (dhcp::params) - class can be used for client and server

--- manifests/definitions/dhcp-host.pp
+++ manifests/definitions/dhcp-host.pp
@@ -1,9 +1,20 @@
+/*
+
+= Definition: dhcp::host
+Create dhcp configuration for a host
+
+Arguments:
+ *$mac*:           host MAC address (mandatory)
+ *$subnet*:        subnet in which we want to add this host
+ *$fixed_address*: host fixed address (if not set, takes $name)
+
+*/
 define dhcp::host($ensure=present,$mac,$subnet,$fixed_address=false) {
-  include dhcp::variables
-  common::concatfilepart {$name:
+  include dhcp::params
+  common::concatfilepart {"dhcp.host.$name":
     ensure => $ensure,
     notify => Service["dhcpd"],
-    file   => "${dhcp::variables::config_dir}/hosts.d/${subnet}.conf",
+    file   => "${dhcp::params::config_dir}/hosts.d/${subnet}.conf",
     require => Dhcp::Subnet[$subnet],
     content => template("dhcp/host.conf.erb"),
   }
