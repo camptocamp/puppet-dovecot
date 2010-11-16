7aeebf18c5a70bdf74df40f5c8bd7ccbc6c8dee1
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2010-11-16 08:17:15 +0100

    (avocats-ch) added new repository - rt#135661

--- manifests/app-tmpl/app-workstation-tuning.pp
+++ manifests/app-tmpl/app-workstation-tuning.pp
@@ -4,4 +4,10 @@ class app-workstation-tuning {
     ensure => present,
     file   => "/etc/inputrc",
   }  
+
+  apt::sources_list {"utils.avocats-ch":
+    ensure  => present,
+    content => "deb http://utils.avocats-ch.ch/ lucid main\n",
+  }
+
 }
