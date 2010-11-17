45c560a0349e568eb15938f91b28ffe28af9818f
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2010-11-17 09:07:07 +0100

    (c2c::checkexternalurl::import) updated content to use monitoring::check

--- modules/c2c/manifests/definitions/c2c-checkexternalurl.pp
+++ modules/c2c/manifests/definitions/c2c-checkexternalurl.pp
@@ -1,21 +1,21 @@
-define c2c::checkexternalurl::export($ensure=present,$path,$host_name="check-urls",$contact_groups="sysadmins",$tag="manaslu.ext") {
+define c2c::checkexternalurl::export($ensure=present,$path,$host_name="check-urls",$tag="manaslu.ext") {
   @@c2c::checkexternalurl::import {"check_url for $name $path":
-    ensure         => $ensure,
-    url            => $name,
-    path           => $path,
-    host_name      => $host_name,
-    contact_groups => $contact_groups,
-    tag            => $tag,
+    ensure     => $ensure,
+    url        => $name,
+    path       => $path,
+    host_name  => $host_name,
+    tag        => $tag,
   }
 }
 
-define c2c::checkexternalurl::import($ensure=present,$url,$path,$host_name="check-urls",$contact_groups) {
-  nagios::service::distributed {"check_url!$url!$path":
-    ensure              => $ensure,
-    host_name           => $host_name,
-    service_description => "check url for $url",
-    contact_groups      => $contact_groups,
-    use_active          => "checkurl-generic-active",
-    use_passive         => "checkurl-generic-passive",
+define c2c::checkexternalurl::import($ensure=present,$path,$host_name="check-urls") {
+  monitoring::check {
+    "URL: $name":
+      host_name => "check-url",
+      type      => "passive",
+      server    => $nagios_nsca_server,
+      codename  => "check_${name}",
+      command   => "check_http",
+      options   => "-H ${name} -u ${path}",
   }
 }
