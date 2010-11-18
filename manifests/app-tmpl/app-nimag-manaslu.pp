c1e41f69965913d8e5330bd63a73585ae864f268
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2010-11-18 08:39:46 +0100

    (app-nimag-manaslu) changed hostname for check-url so that we know where it comes from

--- manifests/app-tmpl/app-nimag-manaslu.pp
+++ manifests/app-tmpl/app-nimag-manaslu.pp
@@ -1,12 +1,11 @@
 class app-nimag-manaslu {
-  nagios::host::nsca {"check-url":
+  nagios::host::nsca {"check-url-c2c":
     ensure         => present,
     export_for     => "nagios-${nagios_nsca_server}",
     hostgroups     => "${operatingsystem}${lsbmajdistrelease}, ${virtual}, ${hardwaremodel}, ${server_group}, external",
     contact_groups => $basic_contact_group,
-    nagios_alias   => "check-url",
+    nagios_alias   => "C2C URL Checks",
   }
-
 
   nagios::template {"checkurl-generic-active":
     ensure => present,
@@ -18,33 +17,33 @@ class app-nimag-manaslu {
   }
 
   c2c::checkexternalurl::import{
-    "geoportal.unhcr.org":            path => "/";
-    "trac.mapfish.org":               path => "/tracdocs/js/trac.js";
-    "www.secureows.org":              path => "/trac/secureows/chrome/common/js/trac.js";
-    "www.riv-agridea.ch":             path => "/css/main.css";
-    "riv-agridea.ch":                 path => "/css/main.css";
-    "acorda-maquette.camptocamp.net": path => "/";
-    "c2cpc36.camptocamp.com":         path => "/";
-    "www.lausannejardins.ch":         path => "/site/images/stories/2009/lausanne_jardin_entree.gif";
-    "lausannejardins.ch":             path => "/site/images/stories/2009/lausanne_jardin_entree.gif";
-    "tinyerp.saas-camptocamp.com":    path => "/tracdocs/js/trac.js";
-    "ecointesys.saas-camptocamp.com": path => "/";
-    "www.reseauajema.ch":             path => "/components/com_rsform/front.css";
-    "www.ajema.ch":                   path => "/components/com_rsform/front.css";
-    "reseauajema.ch":                 path => "/components/com_rsform/front.css";
-    "ajema.ch":                       path => "/components/com_rsform/front.css";
-    "vanoise.camptocamp.net":         path => "/media/system/js/caption.js";
-    "extranet.camptocamp.net":        path => "/media/system/js/caption.js";
-    "observatoire.camptocamp.net":    path => "/media/system/js/caption.js";
-    "c2cpc42.camptocamp.com":         path => "/cdc/scripts/mapfish/mfbase/ext/adapter/ext/ext-base.js";
-    "swissrivers.ch":                 path => "/mfbase/ext/adapter/ext/ext-base.js";
-    "www.swissrivers.ch":             path => "/mfbase/ext/adapter/ext/ext-base.js";
-    "project.camptocamp.com":         path => "/img/next.gif";
-    "www.geoext.org":                 path => "/_static/geoext.css";
-    "geoext.org":                     path => "/_static/geoext.css";
-    "c2cpc49.camptocamp.com":         path => "/_static/geoext.css";
-    "trac.geoext.org":                path => "/tracdocs/css/trac.css";
-    "128.179.66.26":                  path => "/";
-    "sa.camptocamp.com":              path => "/";
+    "geoportal.unhcr.org":            host_name => "check-url-c2c", path => "/";
+    "trac.mapfish.org":               host_name => "check-url-c2c", path => "/tracdocs/js/trac.js";
+    "www.secureows.org":              host_name => "check-url-c2c", path => "/trac/secureows/chrome/common/js/trac.js";
+    "www.riv-agridea.ch":             host_name => "check-url-c2c", path => "/css/main.css";
+    "riv-agridea.ch":                 host_name => "check-url-c2c", path => "/css/main.css";
+    "acorda-maquette.camptocamp.net": host_name => "check-url-c2c", path => "/";
+    "c2cpc36.camptocamp.com":         host_name => "check-url-c2c", path => "/";
+    "www.lausannejardins.ch":         host_name => "check-url-c2c", path => "/site/images/stories/2009/lausanne_jardin_entree.gif";
+    "lausannejardins.ch":             host_name => "check-url-c2c", path => "/site/images/stories/2009/lausanne_jardin_entree.gif";
+    "tinyerp.saas-camptocamp.com":    host_name => "check-url-c2c", path => "/tracdocs/js/trac.js";
+    "ecointesys.saas-camptocamp.com": host_name => "check-url-c2c", path => "/";
+    "www.reseauajema.ch":             host_name => "check-url-c2c", path => "/components/com_rsform/front.css";
+    "www.ajema.ch":                   host_name => "check-url-c2c", path => "/components/com_rsform/front.css";
+    "reseauajema.ch":                 host_name => "check-url-c2c", path => "/components/com_rsform/front.css";
+    "ajema.ch":                       host_name => "check-url-c2c", path => "/components/com_rsform/front.css";
+    "vanoise.camptocamp.net":         host_name => "check-url-c2c", path => "/media/system/js/caption.js";
+    "extranet.camptocamp.net":        host_name => "check-url-c2c", path => "/media/system/js/caption.js";
+    "observatoire.camptocamp.net":    host_name => "check-url-c2c", path => "/media/system/js/caption.js";
+    "c2cpc42.camptocamp.com":         host_name => "check-url-c2c", path => "/cdc/scripts/mapfish/mfbase/ext/adapter/ext/ext-base.js";
+    "swissrivers.ch":                 host_name => "check-url-c2c", path => "/mfbase/ext/adapter/ext/ext-base.js";
+    "www.swissrivers.ch":             host_name => "check-url-c2c", path => "/mfbase/ext/adapter/ext/ext-base.js";
+    "project.camptocamp.com":         host_name => "check-url-c2c", path => "/img/next.gif";
+    "www.geoext.org":                 host_name => "check-url-c2c", path => "/_static/geoext.css";
+    "geoext.org":                     host_name => "check-url-c2c", path => "/_static/geoext.css";
+    "c2cpc49.camptocamp.com":         host_name => "check-url-c2c", path => "/_static/geoext.css";
+    "trac.geoext.org":                host_name => "check-url-c2c", path => "/tracdocs/css/trac.css";
+    "128.179.66.26":                  host_name => "check-url-c2c", path => "/";
+    "sa.camptocamp.com":              host_name => "check-url-c2c", path => "/";
   }
 }
