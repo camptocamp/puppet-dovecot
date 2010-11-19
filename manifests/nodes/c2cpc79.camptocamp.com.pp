9f0abe1beee3ea99e710c807e9b6aa70d0f82193
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2010-11-19 09:05:06 +0100

    (mecatis openerp hosted) migrated c2cpc79 from sa

---
+++ manifests/nodes/c2cpc79.camptocamp.com.pp
@@ -0,0 +1,7 @@
+node "c2cpc79.camptocamp.com" {
+  $ps1label = "mecatis hosted"
+  $openerp_project = 'mecatis'
+  $openerp_client_contact = 's.simon@mecatis.ch'
+
+  include srv-c2c-openerp-hosted-prod
+}
