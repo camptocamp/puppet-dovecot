ad0b4170f91635c0ea11ce4c93e9185538bee47a
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2010-11-19 14:14:59 +0100

    (are-hosting) migrated c2cpc76 from sa.camptocamp.com

---
+++ manifests/srv-tmpl/srv-c2c-are-hosting.pp
@@ -0,0 +1,20 @@
+class srv-c2c-are-hosting {
+  ### Global attributes ##########################
+  $server_group = "prod"
+  $ps1label = "are hosting"
+  $sudo_apache_admin_user = "%sigdev"
+  $sudo_postgresql_admin_user = " %sigdev"
+  $sudo_mysql_admin_user = " %sigdev"
+
+  ### OS #########################################
+  include os-base
+  include os-server
+
+  ### MW #########################################
+  include mw-apache
+  include mw-mysql
+  include mw-sig
+
+  include app-c2c-are-hosting
+
+}
