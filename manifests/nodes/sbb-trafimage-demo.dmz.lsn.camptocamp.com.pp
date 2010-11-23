abbb4b93daa0f51eed24a2e719819743b5d8c688
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2010-11-23 12:16:12 +0100

    new nodes: sbb-trafimage-dev and demo

---
+++ manifests/nodes/sbb-trafimage-demo.dmz.lsn.camptocamp.com.pp
@@ -0,0 +1,4 @@
+node "sbb-trafimage-demo.dmz.lsn.camptocamp.com" {
+  $apache_vhost_name = "sbb_trafimage"
+  include srv-c2c-sig-dev
+}
---
+++ manifests/nodes/sbb-trafimage-dev.int.lsn.camptocamp.com.pp
@@ -0,0 +1,4 @@
+node "sbb-trafimage-dev.int.lsn.camptocamp.com" {
+  $apache_vhost_name = "sbb_trafimage"
+  include srv-c2c-sig-dev
+}
