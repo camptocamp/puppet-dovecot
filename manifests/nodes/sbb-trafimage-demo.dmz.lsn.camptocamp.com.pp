38bbbf64a08cffeeca875062785ef3a19051ad3f
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2010-11-23 12:27:38 +0100

    corrected vhost on sbb-trafimage

--- manifests/nodes/sbb-trafimage-demo.dmz.lsn.camptocamp.com.pp
+++ manifests/nodes/sbb-trafimage-demo.dmz.lsn.camptocamp.com.pp
@@ -1,4 +1,3 @@
 node "sbb-trafimage-demo.dmz.lsn.camptocamp.com" {
-  $apache_vhost_name = "sbb_trafimage"
   include srv-c2c-sig-dev
 }
--- manifests/nodes/sbb-trafimage-dev.int.lsn.camptocamp.com.pp
+++ manifests/nodes/sbb-trafimage-dev.int.lsn.camptocamp.com.pp
@@ -1,4 +1,3 @@
 node "sbb-trafimage-dev.int.lsn.camptocamp.com" {
-  $apache_vhost_name = "sbb_trafimage"
   include srv-c2c-sig-dev
 }
