4f73ada56b76edf9a0801e5df76893cfd5a86db2
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2010-12-01 12:32:39 +0100

    dhcp::host - removed require - created depedency loop

--- modules/dhcp/manifests/definitions/dhcp-host.pp
+++ modules/dhcp/manifests/definitions/dhcp-host.pp
@@ -15,7 +15,6 @@ define dhcp::host($ensure=present,$mac,$subnet,$fixed_address=false) {
     ensure => $ensure,
     notify => Service["dhcpd"],
     file   => "${dhcp::params::config_dir}/hosts.d/${subnet}.conf",
-    require => Dhcp::Subnet[$subnet],
     content => template("dhcp/host.conf.erb"),
   }
 }
