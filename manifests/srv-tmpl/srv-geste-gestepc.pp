a938ba1c0d35164a61226c037866e14bff88d5f0
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2010-12-01 12:34:38 +0100

    srv-geste-gestepc - now includes all necessary stuff for geste (dhcp is NOT activated for now)

--- manifests/srv-tmpl/srv-geste-gestepc.pp
+++ manifests/srv-tmpl/srv-geste-gestepc.pp
@@ -7,11 +7,36 @@ class srv-geste-gestepc {
   $server_group = "prod" #(one of dev, demo or prod)
   $is_external = true
 
+  $samba_domain_name = "GESTDOMAIN"
+  $samba_netbios_name = "FILESERVER"
+  $ldap_uri = "ldap://127.0.0.1"
+  $ldap_base = "cn=admin,dc=ldap,dc=geste"
+  $ldap_admin_password = "aic3waeB"
+  $ldap_suffix = "dc=ldap,dc=geste"
+  $slapd_admin_passwd = $ldap_admin_password
+  $slapd_passwd = $ldap_admin_password
+  $slapd_organisation = "GESTE Ingeneer"
+  $slapd_allow_v2 = "false"
+  $slapd_purge_old = "false"
+  $slapd_domain = "ldap.geste"
+
   ### OS #########################################
   include os-base
   include os-server
 
-  ### MW #########################################
 
+  ### MW #########################################
+  include mw-apache-ssl
+  include mw-lamp
+
+  ### APP ########################################
+
+  include geste::network
+  include geste::dns
+#  include geste::dhcp
+  include geste::ldap
+  include geste::samba
+  include geste::mail
+  include geste::webapp
 
 }
