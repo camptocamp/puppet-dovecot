2092a488700865cf5d70a7639b049882df870905
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2010-12-02 11:16:49 +0100

    geste::dns - some cleanup, updates gestepc3 internal IP.

--- site-modules/geste/manifests/classes/geste-dns.pp
+++ site-modules/geste/manifests/classes/geste-dns.pp
@@ -28,41 +28,41 @@ class geste::dns {
     ensure => present,
     zone   => "geste",
     owner  => "gestepc3",
-    host   => "192.168.12.254",
+    host   => "192.168.12.11",
   }
 
   bind::a {
-    "gateway":    zone => "geste", host => "192.168.12.1";
-    "hephaistos": zone => "geste", host => "192.168.12.2";
+    "gateway":      zone => "geste", host => "192.168.12.1";
+    "hephaistos":   zone => "geste", host => "192.168.12.2";
     "backup-geste": zone => "geste", host => "192.168.12.3";
-    "old-server": zone => "geste", host => "192.168.12.4";
-    "gestorage2": zone => "geste", host => "192.168.12.6";
-    "gestorage3": zone => "geste", host => "192.168.12.7";
-    "gestorage4": zone => "geste", host => "192.168.12.8";
-    "openerp":    zone => "geste", host => "192.168.12.9";
-    "poseidon1":  zone => "geste", host => "192.168.12.110";
-    "gestepc11": zone => "geste", host => "192.168.12.111";
-    "gestepc12": zone => "geste", host => "192.168.12.112";
-    "gestepc13": zone => "geste", host => "192.168.12.113";
-    "gestepc14": zone => "geste", host => "192.168.12.114";
-    "gestorage":  zone => "geste", host => "192.168.12.200";
-    "pc-zib":     zone => "geste", host => "192.168.12.201";
-    "pc-gic":     zone => "geste", host => "192.168.12.202";
-    "pc-jbd":     zone => "geste", host => "192.168.12.203";
-    "pc-fev":     zone => "geste", host => "192.168.12.204";
-    "pc-fap":     zone => "geste", host => "192.168.12.205";
-    "pc-ges":     zone => "geste", host => "192.168.12.206";
-    "pc-rho":     zone => "geste", host => "192.168.12.207";
-    "pc-vhu":     zone => "geste", host => "192.168.12.208";
-    "pc-pam":     zone => "geste", host => "192.168.12.209";
-    "pc-mim":     zone => "geste", host => "192.168.12.210";
-    "mac-mim":    zone => "geste", host => "192.168.12.211";
-    "pc-rams":    zone => "geste", host => "192.168.12.212";
-    "pc-dal":     zone => "geste", host => "192.168.12.213";
-    "pc-djo":     zone => "geste", host => "192.168.12.214";
-    "pc-cfa":     zone => "geste", host => "192.168.12.215";
-    "pc-tka":     zone => "geste", host => "192.168.12.216";
-    "zeus":       zone => "geste", host => "192.168.12.10";
+    "old-server":   zone => "geste", host => "192.168.12.4";
+    "gestorage2":   zone => "geste", host => "192.168.12.6";
+    "gestorage3":   zone => "geste", host => "192.168.12.7";
+    "gestorage4":   zone => "geste", host => "192.168.12.8";
+    "openerp":      zone => "geste", host => "192.168.12.9";
+    "zeus":         zone => "geste", host => "192.168.12.10";
+    "poseidon1":    zone => "geste", host => "192.168.12.110";
+    "gestepc11":    zone => "geste", host => "192.168.12.111";
+    "gestepc12":    zone => "geste", host => "192.168.12.112";
+    "gestepc13":    zone => "geste", host => "192.168.12.113";
+    "gestepc14":    zone => "geste", host => "192.168.12.114";
+    "gestorage":    zone => "geste", host => "192.168.12.200";
+    "pc-zib":       zone => "geste", host => "192.168.12.201";
+    "pc-gic":       zone => "geste", host => "192.168.12.202";
+    "pc-jbd":       zone => "geste", host => "192.168.12.203";
+    "pc-fev":       zone => "geste", host => "192.168.12.204";
+    "pc-fap":       zone => "geste", host => "192.168.12.205";
+    "pc-ges":       zone => "geste", host => "192.168.12.206";
+    "pc-rho":       zone => "geste", host => "192.168.12.207";
+    "pc-vhu":       zone => "geste", host => "192.168.12.208";
+    "pc-pam":       zone => "geste", host => "192.168.12.209";
+    "pc-mim":       zone => "geste", host => "192.168.12.210";
+    "mac-mim":      zone => "geste", host => "192.168.12.211";
+    "pc-rams":      zone => "geste", host => "192.168.12.212";
+    "pc-dal":       zone => "geste", host => "192.168.12.213";
+    "pc-djo":       zone => "geste", host => "192.168.12.214";
+    "pc-cfa":       zone => "geste", host => "192.168.12.215";
+    "pc-tka":       zone => "geste", host => "192.168.12.216";
   }
 
   bind::cname {
