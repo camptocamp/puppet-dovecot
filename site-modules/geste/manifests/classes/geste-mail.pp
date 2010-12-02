c3848d42d2e252d4bcb3ba75ed7c57a940306649
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2010-12-02 11:17:25 +0100

    geste::mail - changed how postfix is configured on gestepc*

--- site-modules/geste/manifests/classes/geste-mail.pp
+++ site-modules/geste/manifests/classes/geste-mail.pp
@@ -1,6 +1,4 @@
 class geste::mail {
-  $postfix_relayhost = "psemail.epfl.ch"
-  $valid_fqdn = $fqdn
-  $root_mail_recipient = "cedric.jeanneret@camptocamp.com"
-  include postfix::satellite
+  $root_mail_recipient = "geste@camptocamp.com"
+  include postfix
 }
