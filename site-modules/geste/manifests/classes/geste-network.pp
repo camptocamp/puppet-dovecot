91eb0ac002cb090b889a64363fc083e86d52a99c
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2010-12-03 13:49:52 +0100

    geste::network - instead of using templates, use static files.
    Easier to know whet they do.

--- site-modules/geste/manifests/classes/geste-network.pp
+++ site-modules/geste/manifests/classes/geste-network.pp
@@ -1,17 +1,20 @@
 class geste::network {
   file {"/etc/network/interfaces":
-    ensure  => present,
-    owner   => root,
-    group   => root,
-    mode    => 0644,
-    content => template("geste/interfaces.erb"),
+    ensure => present,
+    owner  => root,
+    group  => root,
+    mode   => 0644,
+    source => $geste_master? {
+      true  => "puppet:///modules/geste/network/interfaces.master",
+      false => "puppet:///modules/geste/network/interfaces.slave",
+    }
   }
 
   file {"/etc/resolv.conf":
-    ensure  => present,
-    owner   => root,
-    group   => root,
-    mode    => 0644,
-    content => template("geste/resolv.conf.erb"),
+    ensure => present,
+    owner  => root,
+    group  => root,
+    mode   => 0644,
+    source => "puppet:///modules/geste/network/resolv.conf",
   }
 }
