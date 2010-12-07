313989f6cc5927e8208277c453c66a2585b9eb90
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2010-12-07 07:44:18 +0100

    monitoring::updates - using new "sudo" argument for monitoring::check

--- modules/monitoring/manifests/classes/monitoring-updates.pp
+++ modules/monitoring/manifests/classes/monitoring-updates.pp
@@ -14,19 +14,13 @@ class monitoring::updates {
         ensure => present,
         require => File["${monitoring::params::customplugins}check_system_update.pl"],
         codename => "check_system_update",
-        base     => "/usr/bin/",
-        command  => "sudo",
-        options  => "${monitoring::params::customplugins}check_system_update.pl",
+        base     => "${monitoring::params::customplugins}",
+        sudo     => "root",
+        command  => "check_system_update.pl",
         type     => "passive",
         server   => $nagios_nsca_server,
         interval => "1440", # once a day
         retry    => "1440", # once a day
-      }
-
-      common::concatfilepart {"monitoring.check_system_update":
-        ensure => present,
-        file   => "/etc/sudoers",
-        content => "nagios ALL=(ALL) ${monitoring::params::customplugins}check_system_update.pl\n",
       }
 
     }
