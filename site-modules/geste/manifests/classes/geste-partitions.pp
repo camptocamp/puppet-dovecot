d78b6c3b7638e93f7b3db66b7e3bc0ecbd5e21e0
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2010-12-10 16:21:26 +0100

    geste::partitions - added new partition for xapian indexes

--- site-modules/geste/manifests/classes/geste-partitions.pp
+++ site-modules/geste/manifests/classes/geste-partitions.pp
@@ -47,14 +47,29 @@ class geste::partitions {
     require => Logical_volume["misc"],
   }
 
+  logical_volume {"xapian":
+    ensure  => present,
+    size    => "15G",
+    require => Volume_group["vg1"],
+    volume_group => "vg1",
+  }
+
+  filesystem {"/dev/vg1/xapian":
+    ensure  => present,
+    fs_type => "ext4",
+    require => Logical_volume["xapian"],
+  }
+
   file {
     "/srv/misc":         ensure => directory, owner => root, group => root, mode => 0755;
     "/srv/computations": ensure => directory, owner => root, group => root, mode => 0755;
+    "/srv/xapian":       ensure => directory, owner => root, group => root, mode => 0755;
   }
 
   mount {
-    "/home":             ensure => mounted, device => "/dev/vg1/home", require => Filesystem["/dev/vg1/home"], fstype => "ext4", options => "auto,defaults";
-    "/srv/misc":         ensure => mounted, device => "/dev/vg1/misc", require => Filesystem["/dev/vg1/misc"], fstype => "ext4", options => "auto,defaults";
+    "/home":             ensure => mounted, device => "/dev/vg1/home",   require => Filesystem["/dev/vg1/home"],   fstype => "ext4", options => "auto,defaults";
+    "/srv/misc":         ensure => mounted, device => "/dev/vg1/misc",   require => Filesystem["/dev/vg1/misc"],   fstype => "ext4", options => "auto,defaults";
+    "/srv/xapian":       ensure => mounted, device => "/dev/vg1/xapian", require => Filesystem["/dev/vg1/xapian"], fstype => "ext4", options => "auto,defaults";
     "/srv/computations": ensure => mounted, device => "/dev/vg1/computations", require => Filesystem["/dev/vg1/computations"], fstype => "ext4", options => "auto,defaults";
   }
 
