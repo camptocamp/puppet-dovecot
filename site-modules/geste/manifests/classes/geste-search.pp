2fa78c01efb7a2e96d4bd04a3673951002b1b460
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2010-12-10 16:21:51 +0100

    geste::search - added new packages and index directories

--- site-modules/geste/manifests/classes/geste-search.pp
+++ site-modules/geste/manifests/classes/geste-search.pp
@@ -17,4 +17,25 @@ class geste::search {
     "python-dns",
     ]: ensure => present,
   }
+
+  # packages for omega filters
+  package {[
+      "antiword",
+      "xpdf-utils",
+      "catdoc",
+    ]: ensure => latest,
+  }
+
+  file {[
+    "/srv/xapian/administration",
+    "/srv/xapian/engineering",
+    "/srv/xapian/public",
+    ]:
+    ensure => directory,
+    owner  => root,
+    group  => root,
+    mode   => 0755,
+    require => Mount["/srv/xapian"],
+  }
+
 }
