1363c958ead2700227510a22860dbc87d2853e4a
Jerome Bove <jerome.bove@camptocamp.com>
2010-12-10 10:55:35 +0100

    Include srv-c2c-sig-perso template in formation-ch6 for ochriste' need

--- manifests/nodes/formation-ch6.wrk.lsn.camptocamp.com.pp
+++ manifests/nodes/formation-ch6.wrk.lsn.camptocamp.com.pp
@@ -1,3 +1,6 @@
 node 'formation-ch6.wrk.lsn.camptocamp.com' {
-  include wrk-formation
+#  include wrk-formation
+
+# Installation du template sig pour le besoin de ochriste
+  include srv-c2c-sig-perso
 }
