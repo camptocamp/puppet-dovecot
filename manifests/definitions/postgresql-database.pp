a6552597deaedfbd64081f00e95689cbaf6b69cb
Marc Fournier <marc.fournier@camptocamp.com>
2010-12-21 21:12:38 +0100

    postgresql::database - replaced weak regex by an sql query.
    This avoids failure when a user has the same name as a database.

--- manifests/definitions/postgresql-database.pp
+++ manifests/definitions/postgresql-database.pp
@@ -28,15 +28,15 @@ define postgresql::database(
       exec { "Create $name postgres db":
         command => "/usr/bin/createdb $ownerstring $encodingstring $name -T $template",
         user    => "postgres",
-        unless  => "/usr/bin/psql -l | grep '$name  *|'",
+        unless  => "test \$(psql -tA -c \"SELECT count(*)=1 FROM pg_catalog.pg_database where datname='${name}';\") = t",
         require => Service["postgresql"],
       }
     }
     absent:  {
       exec { "Remove $name postgres db":
         command => "/usr/bin/dropdb $name",
-        onlyif  => "/usr/bin/psql -l | grep '$name  *|'",
         user    => "postgres",
+        onlyif  => "test \$(psql -tA -c \"SELECT count(*)=1 FROM pg_catalog.pg_database where datname='${name}';\") = t",
         require => Service["postgresql"],
       }
     }
