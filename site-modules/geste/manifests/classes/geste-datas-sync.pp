bf9dfa597a727802804c51b636e0fa3330d8a056
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2011-01-04 14:43:06 +0100

    geste::data-sync (and script) - push datas from master -> slave

---
+++ site-modules/geste/manifests/classes/geste-datas-sync.pp
@@ -0,0 +1,26 @@
+class geste::datas-sync {
+  if $geste_master {
+    file {"/usr/local/sbin/sync-datas":
+      ensure => present,
+      owner  => root,
+      group  => root,
+      mode   => 0700,
+      source => "puppet:///modules/geste/sync-datas",
+    }
+
+    file {"/var/log/sync-datas":
+      ensure => directory,
+      owner  => root,
+      group  => root,
+      mode   => 0755,
+    }
+
+    cron {"sync datas to slave":
+      ensure => present,
+      command => "/usr/local/sbin/sync-datas",
+      hour    => "1",
+      minute  => ip_to_cron(),
+      require => File["/usr/local/sbin/sync-datas"],
+    }
+  }
+}
