a27b2ba8a41238defb5481e37c0ccb760f8b13ce
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2011-01-14 08:53:35 +0100

    dovecot - definition import

---
+++ manifests/definitions/dovecot-configuration.pp
@@ -0,0 +1,38 @@
+/*
+
+= Definition: dovecot::configuration
+Allow you to add new dovecot configuration.
+
+Args:
+  *$source*     source file, like puppet:///modules/foo/bar.conf
+  *$content*    either inline content, or template
+
+Note: you an only provide $content OR $source, not both of them!
+
+*/
+define dovecot::configuration($ensure=present,$source=false,$content=false) {
+  if !($content or $source) {
+    fail 'Please provide either $source or $content'
+  }
+  if ($content and $source) {
+    fail 'Please provide either $source OR $content'
+  }
+
+  file {"/etc/dovecot/conf.d/${name}.conf":
+    ensure => $ensure,
+    mode   => 0644,
+    owner  => root,
+    group  => root,
+    notify => Exec["reload dovecot"],
+  }
+
+  if $content {
+    File["/etc/dovecot/conf.d/${name}.conf"] {
+      content => $content,
+    }
+  } else {
+    File["/etc/dovecot/conf.d/${name}.conf"] {
+      source => $source,
+    }
+  }
+}
