e49c841e14da7a10b9b02cb8f6771c3f889de726
Rudi Wijaya <rudi@berbatik.com>
2012-05-04 15:12:23 +0700

    Fix dependency LDAP

--- manifests/base.pp
+++ manifests/base.pp
@@ -13,7 +13,7 @@ class dovecot::base {
     fail 'Please provide either $dovecot_auth_ldap or $dovecot_auth_pam or $dovecot_auth_database'
   }
 
-  if ! ($dovecot_ldap_host or $dovecot_ldap_uri ) {
+  if ($dovecot_auth_ldap and ! ($dovecot_ldap_host or $dovecot_ldap_uri )) {
     fail 'Please provide either $dovecot_ldap_host or $dovecot_ldap_uri'
   }
 
